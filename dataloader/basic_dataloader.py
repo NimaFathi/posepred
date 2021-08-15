0fddf080d681ddb0d875e6f1f9c5cdd6964fc539
rmool <armin2saadat@gmail.com>
2021-08-16 00:02:56 +0430

    add mask to dataloader.

--- dataloader/basic_dataloader.py
+++ dataloader/basic_dataloader.py
@@ -15,38 +15,50 @@ class Dataloader(Dataset):
                 print("Exception of type", type(e), "occurred: ", e.args)
                 continue
         self.data = data.copy().reset_index(drop=True)
+        seq = self.data.iloc[0]
+        self.obs_frames_num = len(seq.observed_pose[0])
+        self.future_frames_num = len(seq.future_pose[0])
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, index):
         seq = self.data.iloc[index]
-        assert len(seq.observed_pose) == len(seq.future_pose), "number of persons must be equal in observed and future frames."
+        assert len(seq.observed_pose) == len(
+            seq.future_pose), "number of persons must be equal in observed and future frames."
+
+        seq_persons_num = len(seq.observed_pose)
 
         outputs = []
-        for index_p, obs_seq_person in seq.observed_pose:
+        for person_index in range(seq_persons_num):
             outputs_p = []
 
-            obs_pose = torch.tensor([obs_seq_person[i] for i in range(0, self.args.input, self.args.skip)])
+            person_obs_pose_frames = seq.observed_pose[person_index]
+            obs_pose = torch.tensor([person_obs_pose_frames[i] for i in range(0, self.obs_frames_num, self.args.skip + 1)])
             obs_vel = (obs_pose[1:] - obs_pose[:-1])
 
-            future_seq_person = seq.future_pose[index_p]
-            future_pose = torch.tensor([future_seq_person for i in range(0, self.args.output, self.args.skip)])
+            person_future_pose_frames = seq.future_pose[person_index]
+            future_pose = torch.tensor(
+                [person_future_pose_frames[i] for i in range(0, self.future_frames_num, self.args.skip + 1)])
             future_vel = torch.cat((future_pose[0] - obs_pose[-1]).unsqueeze(0), future_pose[1:] - future_pose[:-1])
 
             outputs_p.append(obs_pose)
             outputs_p.append(obs_vel)
             outputs_p.append(future_pose)
             outputs_p.append(future_vel)
+
+            if self.args.use_mask:
+                person_obs_mask_frames = seq.observed_mask[person_index]
+                obs_mask = torch.tensor([person_obs_mask_frames[i] for i in range(0, self.obs_frames_num, self.args.skip + 1)])
+
+                person_future_mask_frames = seq.future_mask[person_index]
+                true_mask = torch.tensor([person_future_mask_frames[i] for i in range(0, self.future_frames_num, self.args.skip + 1)])
+
+                outputs.append(obs_mask)
+                outputs.append(true_mask)
+
             outputs.append(outputs_p)
-
-        if self.fname == "posetrack_":
-            obs_mask = torch.tensor([seq.Mask[i] for i in range(0, self.args.output, self.args.skip)])
-            true_mask = torch.tensor([seq.Future_Mask[i] for i in range(0, self.args.output, self.args.skip)])
-            outputs.append(obs_mask)
-            outputs.append(true_mask)
         return tuple(outputs)
-
 
 # def data_loader_lstm_vel(args, data, fname):
 #     dataset = Dataloader(args, data, fname)
@@ -67,4 +79,3 @@ class Dataloader(Dataset):
 #
 # seq = self_data.iloc[0]
 # print(seq.observed_pose)
-
