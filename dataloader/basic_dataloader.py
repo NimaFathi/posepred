64ee602689c45e16b11f91a8fb14328f5f1188c0
rmool <armin2saadat@gmail.com>
2021-08-16 00:09:27 +0430

    refactor dataset and dataloader.

--- dataloader/basic_dataloader.py
+++ dataloader/basic_dataloader.py
@@ -4,7 +4,7 @@ import pandas as pd
 from ast import literal_eval
 
 
-class Dataloader(Dataset):
+class Basic_Dataset(Dataset):
     def __init__(self, args):
         self.args = args
         data = pd.read_csv('../preprocessed_data/simple_dataset.csv')
@@ -26,7 +26,6 @@ class Dataloader(Dataset):
         seq = self.data.iloc[index]
         assert len(seq.observed_pose) == len(
             seq.future_pose), "number of persons must be equal in observed and future frames."
-
         seq_persons_num = len(seq.observed_pose)
 
         outputs = []
@@ -34,7 +33,8 @@ class Dataloader(Dataset):
             outputs_p = []
 
             person_obs_pose_frames = seq.observed_pose[person_index]
-            obs_pose = torch.tensor([person_obs_pose_frames[i] for i in range(0, self.obs_frames_num, self.args.skip + 1)])
+            obs_pose = torch.tensor(
+                [person_obs_pose_frames[i] for i in range(0, self.obs_frames_num, self.args.skip + 1)])
             obs_vel = (obs_pose[1:] - obs_pose[:-1])
 
             person_future_pose_frames = seq.future_pose[person_index]
@@ -49,24 +49,24 @@ class Dataloader(Dataset):
 
             if self.args.use_mask:
                 person_obs_mask_frames = seq.observed_mask[person_index]
-                obs_mask = torch.tensor([person_obs_mask_frames[i] for i in range(0, self.obs_frames_num, self.args.skip + 1)])
-
+                obs_mask = torch.tensor(
+                    [person_obs_mask_frames[i] for i in range(0, self.obs_frames_num, self.args.skip + 1)])
                 person_future_mask_frames = seq.future_mask[person_index]
-                true_mask = torch.tensor([person_future_mask_frames[i] for i in range(0, self.future_frames_num, self.args.skip + 1)])
-
+                true_mask = torch.tensor(
+                    [person_future_mask_frames[i] for i in range(0, self.future_frames_num, self.args.skip + 1)])
                 outputs.append(obs_mask)
                 outputs.append(true_mask)
-
             outputs.append(outputs_p)
+
         return tuple(outputs)
 
-# def data_loader_lstm_vel(args, data, fname):
-#     dataset = Dataloader(args, data, fname)
-#     dataloader = DataLoader(
-#         dataset, batch_size=args.batch_size, shuffle=args.loader_shuffle,
-#         pin_memory=args.pin_memory)
-#     return dataloader
 
+def basic_dataloader(args):
+    dataset = Basic_Dataset(args)
+    dataloader = DataLoader(
+        dataset, batch_size=args.batch_size, shuffle=args.loader_shuffle,
+        pin_memory=args.pin_memory)
+    return dataloader
 
 # data = pd.read_csv('../preprocessed_data/simple_dataset.csv')
 # for col in list(data.columns.values):
