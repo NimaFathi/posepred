697a896291c51a060bbcd859d790009cc894729f
rmool <armin2saadat@gmail.com>
2021-08-17 02:17:37 +0430

    refactor reproter

--- train/reporter.py
+++ train/reporter.py
@@ -6,56 +6,26 @@ from utils.average_meter import AverageMeter
 class Reporter:
 
     def __init__(self):
-        self.train_time = time.time()
-        self.train = dict()
-        self.train['vel_loss'] = AverageMeter()
-        self.train['mask_loss'] = AverageMeter()
-        self.train['mask_acc'] = AverageMeter()
-        self.train['ADE'] = AverageMeter()
-        self.train['FDE'] = AverageMeter()
+        self.attrs = dict()
+        self.attrs['vel_loss'] = AverageMeter()
+        self.attrs['mask_loss'] = AverageMeter()
+        self.attrs['mask_acc'] = AverageMeter()
+        self.attrs['ADE'] = AverageMeter()
+        self.attrs['FDE'] = AverageMeter()
+        self.time = time.time()
 
-        self.validation_time = time.time()
-        self.validation = dict()
-        self.validation['vel_loss'] = AverageMeter()
-        self.validation['mask_loss'] = AverageMeter()
-        self.validation['mask_acc'] = AverageMeter()
-        self.validation['ADE'] = AverageMeter()
-        self.validation['FDE'] = AverageMeter()
-
-    def update_train(self, batch_size, values):
-        self.train_time = time.time() - self.train_time
-        for i, avg_meter in enumerate(self.train.values()):
-            avg_meter.update(values[i], batch_size)
-
-    def update_validation(self, batch_size, values):
-        self.validation_time = time.time() - self.validation_time
-        for i, avg_meter in enumerate(self.validation.values()):
+    def update(self, batch_size, values):
+        self.time = time.time() - self.time
+        for i, avg_meter in enumerate(self.attrs.values()):
             avg_meter.update(values[i], batch_size)
 
     def reset(self):
-        self.train_time = time.time()
-        for i, avg_meter in enumerate(self.train.values()):
-            avg_meter.reset()
-        self.validation_time = time.time()
-        for i, avg_meter in enumerate(self.validation.values()):
+        self.time = time.time()
+        for i, avg_meter in enumerate(self.attrs.values()):
             avg_meter.reset()
 
-    def print_train_values(self):
-        print("time:", self.train_time)
-        for key, avg_meter in self.train.items():
+    def print_values(self):
+        print("time:", self.time)
+        for key, avg_meter in self.attrs.items():
             print(key + ":", avg_meter.get_average())
         print('-' * 30)
-
-    def print_validation_values(self):
-        print("time:", self.validation_time)
-        for key, avg_meter in self.validation.items():
-            print(key + ":", avg_meter.get_average())
-        print('-' * 30)
-
-
-reporter = Reporter()
-reporter.print_train_values()
-reporter.update_train(1, [1, 2, 3, 4, 5])
-reporter.print_train_values()
-reporter.update_train(2, [0, 2, 0, 4, 5])
-reporter.print_train_values()
