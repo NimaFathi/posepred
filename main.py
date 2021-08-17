8f72b7c5cd5300b762033dc174c964c25f851ed8
rmool <armin2saadat@gmail.com>
2021-08-18 02:17:32 +0430

    add pose_from_vel

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ from data_loader.data_loader import get_dataloader
 
 if __name__ == '__main__':
     args = DataloaderArgs('simple_dataset', data_dim=2, is_testing=False, use_mask=False, skip_frame=0,
-                          batch_size=None, shuffle=False, pin_memory=False, num_workers=0)
+                          batch_size=1, shuffle=False, pin_memory=False, num_workers=0)
     dataloader = get_dataloader(args)
 
     for data in dataloader:
-        for i, d in enumerate(data):
-            print(d.shape)
-        break
+        obs_pose = data[0]
+        print(obs_pose.shape)
