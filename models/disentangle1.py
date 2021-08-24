85e88e7f7b03d70fd2ab237e3234e46afdf298a6
rmool <armin2saadat@gmail.com>
2021-08-25 01:15:26 +0430

    edit disentangle

--- models/disentangle1.py
+++ models/disentangle1.py
@@ -9,22 +9,24 @@ class Disentangle1(torch.nn.Module):
         super(Disentangle1, self).__init__()
         self.args = args
 
+        # global
         global_args = args
         global_args.keypoints_num = 1
         self.global_model = ZeroVelocity(global_args).to(torch.device('cuda'))
 
+        # local
         local_args = args
         local_args.keypoints_num = args.keypoints_num - global_args.keypoints_num
         self.local_model = LSTMVel(local_args).to(torch.device('cuda'))
 
-        self.input_size = self.output_size = int(args.keypoints_num * args.keypoint_dim)
-
     def forward(self, inputs):
         outputs = []
         pose, vel = inputs[:2]
-        # [global_pose, global_vel]
+
+        # (global_pose, global_vel)
         global_inputs = [pose[..., : self.args.keypoint_dim], vel[..., : self.args.keypoint_dim]]
-        # [local_pose, local_vel]
+
+        # (local_pose, local_vel)
         local_inputs = [pose[..., self.args.keypoint_dim:], vel[..., self.args.keypoint_dim:]]
 
         if self.args.use_mask:
@@ -33,22 +35,19 @@ class Disentangle1(torch.nn.Module):
             local_inputs.append(pose[..., 1:])
 
         global_outputs = self.global_model(global_inputs)
-        local_inputs = self.local_model(local_inputs)
+        local_outputs = self.local_model(local_inputs)
 
-        for i, global_output in enumerate(global_inputs):
-            local_output = local_inputs[i]
-
-            local_output[...,]
+        # merge local and global outputs
+        global_vel = global_outputs[0]
+        local_vel = local_outputs[0]
+        repeat = torch.ones(len(global_vel.shape), dtype=int)
+        repeat[-1] = self.local_model.args.keypoints_num
+        outputs.append(torch.cat((global_vel, local_vel + global_vel.repeat(tuple(repeat))), dim=-1))
 
         return tuple(outputs)
 
 
-local_ = torch.ones(32, 16, 12)
 
-global_ = torch.ones(32, 16, 3)
 
-repeat = torch.ones(len(global_.shape), dtype=int)
-repeat[-1] = 4
-global_r = global_.repeat(tuple(repeat))
 
-res = torch.cat((global_, local_ + global_r), dim=-1)
+
