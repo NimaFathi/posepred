950ba5cd6aeed5936e25c5f95e88cb206d1afe4e
rmool <armin2saadat@gmail.com>
2021-08-24 06:52:29 +0430

    debug non-interactive dataset

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import numpy as np
 
 if __name__ == '__main__':
 
-    args = TrainingArgs(train_dataset_name='train_PoseTrack', valid_dataset_name='train_PoseTrack',
+    args = TrainingArgs(train_dataset_name='sample_interactive', valid_dataset_name='train_PoseTrack',
                         model_name='lstm_vel', keypoint_dim=2, epochs=6, load_path=None, is_interactive=True,
-                        persons_num=5, use_mask=True, batch_size=7, shuffle=False)
+                        persons_num=5, use_mask=False, batch_size=3, shuffle=False)
 
     train_dataloader_args = DataloaderArgs(args.train_dataset_name, args.keypoint_dim, args.is_interactive,
                                            args.persons_num, args.use_mask, args.is_testing, args.skip_frame,
@@ -15,12 +15,11 @@ if __name__ == '__main__':
 
     train_dataloader = get_dataloader(train_dataloader_args)
 
-    persons = []
-
     for data in train_dataloader:
-        obs_pose = data[0]
-        print(obs_pose.shape)
-        persons.append(obs_pose.shape[1])
+        obs_pose, future_vel, = data[0], data[-1]
+        print(obs_pose.shape, future_vel.shape)
 
-    print(args.train_dataset_name)
-    print('seq_num:', len(persons), '-->   mean:', np.mean(persons), ' std:', np.std(persons))
+    # persons = []
+    # persons.append(obs_pose.shape[1])
+    # print(args.train_dataset_name)
+    # print('seq_num:', len(persons), '-->   mean:', np.mean(persons), ' std:', np.std(persons))
