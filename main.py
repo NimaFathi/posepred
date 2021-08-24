8e4e3f3af7be7e96ffc4406615e998cbb0765aee
rmool <armin2saadat@gmail.com>
2021-08-24 08:18:28 +0430

    trainer is ready for interactive.

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
 from args.training_args import TrainingArgs
 from args.helper import DataloaderArgs
 from data_loader.data_loader import get_dataloader
-import torch
 
 if __name__ == '__main__':
-    ones = torch.ones(2, 4, 5, 2)
-    zeros = torch.zeros(2, 4, 5, 2)
 
-    displacement = 0
-    for d in range(2):
-        displacement += (ones[..., d] - zeros[..., d]) ** 2
-        print(displacement.shape)
-    print(torch.mean(torch.sqrt(displacement)))
-
-    exit()
     args = TrainingArgs(train_dataset_name='sample_interactive', valid_dataset_name='train_PoseTrack',
                         model_name='lstm_vel', keypoint_dim=2, epochs=6, load_path=None, is_interactive=True,
                         persons_num=5, use_mask=False, batch_size=3, shuffle=False)
