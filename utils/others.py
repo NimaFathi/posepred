1ded639c0c345104ac844426e601d25784bdcc1f
rmool <armin2saadat@gmail.com>
2021-08-27 19:24:38 +0430

    refactor pose_from_vel

--- utils/others.py
+++ utils/others.py
@@ -1,17 +1,17 @@
 import torch
 
 
-def pose_from_vel(pred_vel, last_obs_pose, stay_in_frame=False):
-    pred_pose = torch.zeros_like(pred_vel).to('cuda')
+def pose_from_vel(velocity, last_obs_pose, stay_in_frame=False):
+    pose = torch.zeros_like(velocity).to('cuda')
     last_obs_pose_ = last_obs_pose
 
-    for i in range(pred_vel.shape[-2]):
-        pred_pose[..., i, :] = last_obs_pose_ + pred_vel[..., i, :]
-        last_obs_pose_ = pred_pose[..., i, :]
+    for i in range(velocity.shape[-2]):
+        pose[..., i, :] = last_obs_pose_ + velocity[..., i, :]
+        last_obs_pose_ = pose[..., i, :]
 
     if stay_in_frame:
-        for i in range(pred_vel.shape[-1]):
-            pred_pose[..., i] = torch.min(pred_pose[..., i], 1920 * torch.ones_like(pred_pose.shape[:-1]).to('cuda'))
-            pred_pose[..., i] = torch.max(pred_pose[..., i], torch.zeros_like(pred_pose.shape[:-1]).to('cuda'))
+        for i in range(velocity.shape[-1]):
+            pose[..., i] = torch.min(pose[..., i], 1920 * torch.ones_like(pose.shape[:-1]).to('cuda'))
+            pose[..., i] = torch.max(pose[..., i], torch.zeros_like(pose.shape[:-1]).to('cuda'))
 
-    return pred_pose
+    return pose
