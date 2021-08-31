cca9140fd3621358f16516d56a35fc9ce8b11ee1
rmool <armin2saadat@gmail.com>
2021-09-01 03:13:21 +0430

    edit visualization_args.

--- api/visualization.py
+++ api/visualization.py
@@ -1,37 +1,29 @@
-from args.visual_args import VisualArgs
-from args.helper import DataloaderArgs, ModelArgs
+from args.visualization_args import parse_visualization_args
 from data_loader.data_loader import get_dataloader
 from utils.save_load import get_model, load_snapshot
 from visualization.visualizer import Visualizer
 
 if __name__ == '__main__':
 
-    args = VisualArgs(dataset_name='simple_non_interactive_dataset', model_name='zero_velocity', load_path=None,
-                      pred_frames_num=2, keypoint_dim=2, seq_index=5, gif_name='gif-name')
-
-    dataloader_args = DataloaderArgs(args.dataset_name, args.keypoint_dim, args.is_interactive, args.use_mask,
-                                     args.is_testing, args.skip_frame, args.batch_size, args.shuffle, args.pin_memory,
-                                     args.num_workers)
-
+    dataloader_args, model_args, load_path, pred_frames_num, is_testing, seq_index = parse_visualization_args()
     dataloader = get_dataloader(dataloader_args)
 
-    if args.load_path:
-        model, optimizer, epoch, train_reporter, valid_reporter = load_snapshot(args.load_path)
-    elif args.model_name:
-        model_args = ModelArgs(args.model_name, args.use_mask, args.keypoint_dim)
-        model_args.pred_frames_num = args.pred_frames_num if args.is_testing else dataloader.dataset.future_frames_num
+    if load_path:
+        model, optimizer, epoch, train_reporter, valid_reporter = load_snapshot(load_path)
+    elif model_args.model_name:
+        model_args.pred_frames_num = pred_frames_num if is_testing else dataloader.dataset.future_frames_num
         model_args.keypoints_num = dataloader.dataset.keypoints_num
         model = get_model(model_args)
     else:
         raise Exception("Please provide either a model_name or a load_path to a trained model.")
 
     model.zero_grad()
-    data = dataloader.dataset.__getitem__(args.seq_index)
+    data = dataloader.dataset.__getitem__(seq_index)
 
     vis_poses = []
     vis_masks = []
 
-    if not args.is_testing:
+    if not is_testing:
         if model.args.use_mask:
             vis_masks.append(data[-1])
         else:
@@ -47,8 +39,8 @@ if __name__ == '__main__':
         vis_masks.append(data[2])
         vis_masks.append(outputs[1])
 
-    visualizer = Visualizer(dataset=args.dataset_name)
-    if args.keypoint_dim == 2:
+    visualizer = Visualizer(dataset=dataloader_args.dataset_name)
+    if dataloader_args.keypoint_dim == 2:
         visualizer.visualizer_2D(poses=vis_poses, masks=vis_masks)
     else:
         visualizer.visualizer_3D(poses=vis_poses)
