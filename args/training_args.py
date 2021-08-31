fb52f6aeb6578c10001dd0cd3d32ddb31043f3cb
rmool <armin2saadat@gmail.com>
2021-09-01 02:21:33 +0430

    start epoch!

--- args/training_args.py
+++ args/training_args.py
@@ -66,7 +66,7 @@ def __parse_training_args():
 
     # trainer_args
     parser.add_argument('-epochs', type=int, help='number of training epochs')
-    parser.add_argument('-start_epoch', type=int, default=200, help='start epoch')
+    parser.add_argument('-start_epoch', type=int, default=0, help='start epoch')
     parser.add_argument('-lr', type=float, default=0.001, help='learning rate')
     parser.add_argument('-decay_factor', type=float, default=0.95, help='decay_factor for learning_rate')
     parser.add_argument('-decay_patience', type=int, default=20, help='decay_patience for learning_rate')
