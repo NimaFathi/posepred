a8ff3e53095b728d7881c5fbab78d4241c514da0
rmool <armin2saadat@gmail.com>
2021-09-01 02:32:02 +0430

    edit args for evaluation.

--- args/evaluation_args.py
+++ args/evaluation_args.py
@@ -1,5 +1,7 @@
 import argparse
 
+from args.helper import DataloaderArgs, ModelArgs
+
 
 class EvaluationArgs:
     def __init__(self, dataset_name, keypoint_dim, model_name=None, load_path=None, is_interactive=False, persons_num=1,
@@ -18,29 +20,42 @@ class EvaluationArgs:
         self.num_workers = num_workers
         self.is_testing = False
 
+        # model_args
         self.model_name = model_name
         self.load_path = load_path
         self.distance_loss = distance_loss
 
 
 def parse_evaluation_args():
+    args = __parse_evaluation_args()
+    dataloader_args = DataloaderArgs(args.dataset_name, args.keypoint_dim, args.is_interactive, args.persons_num,
+                                     args.use_mask, args.is_testing, args.skip_frame, args.batch_size, args.shuffle,
+                                     args.pin_memory, args.num_workers)
+    model_args = ModelArgs(args.model_name, args.use_mask, args.keypoint_dim)
+
+    return dataloader_args, model_args, args.load_path, args.is_interactive, args.distance_loss
+
+
+def __parse_evaluation_args():
     parser = argparse.ArgumentParser('Evaluation Arguments')
 
     # dataloader_args
-    parser.add_argument('--dataset_name', type=str, help='test_dataset_name')
-    parser.add_argument('--keypoint_dim', type=int, help='dimension of each keypoint')
-    parser.add_argument('--is_interactive', type=bool, default=False, help='support interaction of people')
-    parser.add_argument('--persons_num', type=bool, default=1, help='number of people in each sequence')
-    parser.add_argument('--use_mask', type=bool, default=False, help='visibility mask')
-    parser.add_argument('--skip_frame', type=int, default=0, help='skip frame in reading dataset')
-    parser.add_argument('--batch_size', type=int, default=1, help='batch_size')
-    parser.add_argument('--shuffle', type=bool, default=True)
-    parser.add_argument('--pin_memory', type=bool, default=False)
-    parser.add_argument('--num_workers', type=int, default=0, help='num_workers')
+    parser.add_argument('-dataset_name', type=str, help='test_dataset_name')
+    parser.add_argument('-keypoint_dim', type=int, help='dimension of each keypoint')
+    parser.add_argument('-is_interactive', type=bool, default=False, help='support interaction of people')
+    parser.add_argument('-persons_num', type=bool, default=1, help='number of people in each sequence')
+    parser.add_argument('-use_mask', type=bool, default=False, help='visibility mask')
+    parser.add_argument('-skip_frame', type=int, default=0, help='skip frame in reading dataset')
+    parser.add_argument('-batch_size', type=int, default=1, help='batch_size')
+    parser.add_argument('-shuffle', type=bool, default=True)
+    parser.add_argument('-pin_memory', type=bool, default=False)
+    parser.add_argument('-num_workers', type=int, default=0, help='num_workers')
 
-    parser.add_argument('--model_name', type=str, help='model_name')
-    parser.add_argument('--load_path', type=str, default=None, help='load_path')
-    parser.add_argument('--distance_loss', type=str, default='L1', help='use L1 or L2 as distance loss.')
+    parser.add_argument('-model_name', type=str, help='model_name')
+    parser.add_argument('-load_path', type=str, default=None, help='load_path')
+    parser.add_argument('-distance_loss', type=str, default='L1', help='use L1 or L2 as distance loss.')
 
     evaluation_args = parser.parse_args()
+    evaluation_args.is_testing = False
+
     return evaluation_args
