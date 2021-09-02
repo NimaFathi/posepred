73cde540a0b1296d7bb020e20a59f6bf88c64180
Nima Fathi <nimafathinimafathi@gmail.com>
2021-09-02 17:10:56 +0430

    Add preprocessor api script

--- api/preprocessor.py
+++ api/preprocessor.py
@@ -1,38 +1,13 @@
-import argparse
-
+from args.preprocessor_args import parse_preprocessor_args
 from preprocessor.dpw_preprocessor import Preprocessor3DPW
 from preprocessor.jta_preprocessor import JTAPreprocessor
 from preprocessor.posetrack_preprocessor import PoseTrackPreprocessor
 from preprocessor.somof_3dpw_preprocessor import SoMoF3DPWPreprocessor
 from preprocessor.somof_posetrack_preprocessor import SoMoFPoseTrackPreprocessor
 
-
-def parse_option():
-    parser = argparse.ArgumentParser('argument for predictions')
-    parser.add_argument(
-        '--dataset', type=str,
-        choices=['somof_posetrack', 'posetrack', 'somof_3dpw', '3dpw', 'jta'],
-        default='posetrack'
-    )
-    parser.add_argument('--dataset_path', type=str, default='./raw_data', help='path of dataset')
-    parser.add_argument('--mask', default=False, action='store_true')
-    parser.add_argument('--data_type', type=str, default='train', choices=['train', 'validation', 'test'])
-    parser.add_argument('--custom_name', type=str)
-    parser.add_argument('--is_interactive', default=False, action='store_true')
-    parser.add_argument('--obs_frame_num', type=int, default=16)
-    parser.add_argument('--pred_frame_num', type=int, default=14)
-    parser.add_argument('--skip_frame_num', type=int, default=1)
-    parser.add_argument('--use_video_once', default=False, action='store_true')
-    parser.add_argument('--is_3d', default=False, action='store_true')
-
-    opt = parser.parse_args()
-    opt.device = 'cuda'
-
-    return opt
-
-
 if __name__ == '__main__':
-    opt = parse_option()
+    opt = parse_preprocessor_args()
+
     if opt.dataset == 'posetrack':
         preprocessor = PoseTrackPreprocessor(
             mask=opt.mask, dataset_path=opt.dataset_path,
