5d211d677da0985eaf5a717aa49994b24289a288
rmool <armin2saadat@gmail.com>
2021-09-02 07:21:48 +0430

    add visualization to non-interactive dataset

--- data_loader/non_interactive_dataset.py
+++ data_loader/non_interactive_dataset.py
@@ -36,6 +36,7 @@ class NonInteractiveDataset(Dataset):
             obs_pose = self.get_tensor(seq, 'observed_pose')
             obs_vel = (obs_pose[1:, :] - obs_pose[:-1, :])
             outputs = [obs_pose, obs_vel]
+            outputs_vis = {'obs_pose': obs_pose, 'obs_vel': obs_vel}
         except:
             print('faulty row skipped.')
             return self.__getitem__((index + 1) % self.__len__())
@@ -43,20 +44,38 @@ class NonInteractiveDataset(Dataset):
         if self.use_mask:
             obs_mask = self.get_tensor(seq, 'observed_mask')
             outputs.append(obs_mask)
+            outputs_vis['obs_mask'] = obs_mask
 
         if not self.is_testing:
             future_pose = self.get_tensor(seq, 'future_pose')
             future_vel = torch.cat(
                 ((future_pose[0, :] - obs_pose[-1, :]).unsqueeze(0), future_pose[1:, :] - future_pose[:-1, :]), 0)
             outputs += [future_pose, future_vel]
+            outputs_vis['future_pose'] = future_pose
+            outputs_vis['future_vel'] = future_vel
 
             if self.use_mask:
                 future_mask = self.get_tensor(seq, 'future_mask')
                 outputs.append(future_mask)
+                outputs_vis['future_mask'] = future_mask
+
+        if self.is_visualizing:
+            if 'observed_image_path' in seq.keys():
+                outputs_vis['obs_image'] = seq['observed_image_path']
+            if 'future_image_path' in seq.keys():
+                outputs_vis['future_image'] = seq['future_image_path']
+            if 'observed_cam_extrinsic' in seq.keys():
+                outputs_vis['obs_cam_ex'] = seq['observed_cam_extrinsic']
+            if 'future_cam_extrinsic' in seq.keys():
+                outputs_vis['future_cam_ex'] = seq['future_cam_extrinsic']
+            if 'cam_intrinsic' in seq.keys():
+                outputs_vis['cam_in'] = seq['cam_intrinsic']
+            return outputs_vis
 
         return tuple(outputs)
 
     def get_tensor(self, seq, segment):
         assert segment in seq, 'No segment named: ' + segment
         frames_num = len(seq[segment])
-        return torch.tensor([seq[segment][frame_idx] for frame_idx in range(0, frames_num, self.skip_frame + 1)], dtype=torch.float32)
+        return torch.tensor([seq[segment][frame_idx] for frame_idx in range(0, frames_num, self.skip_frame + 1)],
+                            dtype=torch.float32)
