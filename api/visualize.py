da49e4421982aaf86173f7ad224c77bb8edc3c6f
rmool <armin2saadat@gmail.com>
2021-09-09 03:47:06 +0430

    add dataset_name to visualize.py

--- api/visualize.py
+++ api/visualize.py
@@ -9,7 +9,7 @@ from utils.others import pose_from_vel
 
 if __name__ == '__main__':
 
-    dataloader_args, model_args, load_path, ground_truth, pred_frames_num, index, images_dir = parse_visualization_args()
+    dataloader_args, model_args, load_path, ground_truth, pred_frames_num, index, images_dir, dataset_name = parse_visualization_args()
     dataloader = get_dataloader(dataloader_args)
 
     if load_path:
@@ -74,7 +74,7 @@ if __name__ == '__main__':
     cam_in = data.get('cam_in') if 'cam_in' in data.keys() else None
 
     gif_name = '_'.join((model.args.model_name, dataloader_args.dataset_name, str(index)))
-    visualizer = Visualizer(dataset_name=dataloader_args.dataset_name, images_dir=images_dir)
+    visualizer = Visualizer(dataset_name=dataset_name, images_dir=images_dir)
     if dataloader_args.keypoint_dim == 2:
         visualizer.visualizer_2D(names, poses, masks, images_path, gif_name)
     else:
--- args/visualization_args.py
+++ args/visualization_args.py
@@ -5,19 +5,21 @@ from args.helper import DataloaderArgs, ModelArgs
 
 def parse_visualization_args():
     args = __parse_visualization_args()
-    dataloader_args = DataloaderArgs(args.dataset, args.keypoint_dim, args.interactive, args.persons_num,
+    dataloader_args = DataloaderArgs(args.dataset_file, args.keypoint_dim, args.interactive, args.persons_num,
                                      args.use_mask, args.skip_num, args.batch_size, args.shuffle, args.pin_memory,
                                      args.num_workers, is_testing=not args.ground_truth, is_visualizing=True)
     model_args = ModelArgs(args.model, args.use_mask, args.keypoint_dim)
 
-    return dataloader_args, model_args, args.load_path, args.ground_truth, args.pred_frames_num, args.index, args.images_dir
+    return dataloader_args, model_args, args.load_path, args.ground_truth, args.pred_frames_num, args.index, args.images_dir, args.dataset_name
 
 
 def __parse_visualization_args():
     parser = argparse.ArgumentParser('Visualization Arguments')
 
     # dataloader_args
-    parser.add_argument('--dataset', type=str, help='test_dataset_name')
+    parser.add_argument('--dataset_file', type=str, help='name of the dataset file')
+    parser.add_argument('--dataset_name', type=str, help='name of the dataset',
+                        choices=['somof_posetrack', 'posetrack', 'somof_3dpw', '3dpw', 'jta', 'jaad', 'pie'])
     parser.add_argument('--keypoint_dim', type=int, help='dimension of each keypoint')
     parser.add_argument('--interactive', default=False, action='store_true', help='consider interaction')
     parser.add_argument('--persons_num', type=int, default=1, help='number of people in each sequence')
