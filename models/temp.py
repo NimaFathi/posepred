722807c980a170e1b09a81b0896d38810e434a05
rmool <armin2saadat@gmail.com>
2021-09-19 22:53:45 +0430

    debug

--- models/temp.py
+++ models/temp.py
@@ -7,95 +7,53 @@ import time
 def main(opt):
     # training
     for epo in range(start_epoch, opt.epoch + 1):
-        ret_train = run_model(net_pred, optimizer, is_train=0, data_loader=data_loader, epo=epo, opt=opt)
+        ret_train = run_model(net_pred, optimizer, is_train=0, data_loader=data_loader, opt=opt)
 
-        ret_valid = run_model(net_pred, is_train=1, data_loader=valid_loader, opt=opt, epo=epo)
+        ret_valid = run_model(net_pred, is_train=1, data_loader=valid_loader, opt=opt)
 
-        ret_test = run_model(net_pred, is_train=3, data_loader=test_loader, opt=opt, epo=epo)
+        ret_test = run_model(net_pred, is_train=3, data_loader=test_loader, opt=opt)
 
 
-def run_model(net_pred, optimizer=None, is_train=0, data_loader=None, epo=1, opt=None):
-    if is_train == 0:
-        net_pred.train()
-    else:
-        net_pred.eval()
-
-    l_p3d = 0
-    if is_train <= 1:
-        m_p3d_h36 = 0
-    else:
-        titles = np.array(range(opt.output_n)) + 1
-        m_p3d_h36 = np.zeros([opt.output_n])
-    n = 0
+def run_model(net_pred, optimizer=None, is_train=0, data_loader=None, opt=None):
     in_n = opt.input_n
     out_n = opt.output_n
-    dim_used = np.array([6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 21, 22, 23, 24, 25,
-                         26, 27, 28, 29, 30, 31, 32, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45,
-                         46, 47, 51, 52, 53, 54, 55, 56, 57, 58, 59, 63, 64, 65, 66, 67, 68,
-                         75, 76, 77, 78, 79, 80, 81, 82, 83, 87, 88, 89, 90, 91, 92])
     seq_in = opt.kernel_size
-    # joints at same loc
-    joint_to_ignore = np.array([16, 20, 23, 24, 28, 31])
-    index_to_ignore = np.concatenate((joint_to_ignore * 3, joint_to_ignore * 3 + 1, joint_to_ignore * 3 + 2))
-    joint_equal = np.array([13, 19, 22, 13, 27, 30])
-    index_to_equal = np.concatenate((joint_equal * 3, joint_equal * 3 + 1, joint_equal * 3 + 2))
-
     itera = 1
-    idx = np.expand_dims(np.arange(seq_in + out_n), axis=1) + (
-            out_n - seq_in + np.expand_dims(np.arange(itera), axis=0))
-    st = time.time()
-    for i, (p3d_h36) in enumerate(data_loader):
-        # print(i)
-        batch_size, seq_n, _ = p3d_h36.shape
-        # when only one sample in this batch
-        if batch_size == 1 and is_train == 0:
-            continue
-        n += batch_size
-        bt = time.time()
-        p3d_h36 = p3d_h36.float().cuda()
-        p3d_sup = p3d_h36.clone()[:, :, dim_used][:, -out_n - seq_in:].reshape(
-            [-1, seq_in + out_n, len(dim_used) // 3, 3])
-        p3d_src = p3d_h36.clone()[:, :, dim_used]
-        p3d_out_all = net_pred(p3d_src, input_n=in_n, output_n=out_n, itera=itera)
 
-        p3d_out = p3d_h36.clone()[:, in_n:in_n + out_n]
-        p3d_out[:, :, dim_used] = p3d_out_all[:, seq_in:, 0]
+    for i, (data) in enumerate(data_loader):
+
+        bs, seq_n, _ = data.shape
+
+        sup = data.clone()[:, -(out_n + seq_in):, :].reshape([bs, seq_in + out_n, len(in_n) // 3, 3])
+        src = data.clone()
+        out_all = net_pred(src, input_n=in_n, output_n=out_n, itera=itera)
+
+        p3d_out = data.clone()[:, in_n:in_n + out_n]
+        p3d_out[:, :, dim_used] = out_all[:, seq_in:, 0]
         p3d_out[:, :, index_to_ignore] = p3d_out[:, :, index_to_equal]
         p3d_out = p3d_out.reshape([-1, out_n, 32, 3])
 
-        p3d_h36 = p3d_h36.reshape([-1, in_n + out_n, 32, 3])
+        data = data.reshape([-1, in_n + out_n, 32, 3])
 
-        p3d_out_all = p3d_out_all.reshape([batch_size, seq_in + out_n, itera, len(dim_used) // 3, 3])
+        out_all = out_all.reshape([bs, seq_in + out_n, itera, len(dim_used) // 3, 3])
 
         # 2d joint loss:
-        grad_norm = 0
+
         if is_train == 0:
-            loss_p3d = torch.mean(torch.norm(p3d_out_all[:, :, 0] - p3d_sup, dim=3))
+            loss_p3d = torch.mean(torch.norm(out_all[:, :, 0] - sup, dim=3))
             loss_all = loss_p3d
             optimizer.zero_grad()
             loss_all.backward()
-            nn.utils.clip_grad_norm_(list(net_pred.parameters()), max_norm=opt.max_norm)
             optimizer.step()
+
             # update log values
-            l_p3d += loss_p3d.cpu().data.numpy() * batch_size
+            l_p3d += loss_p3d.cpu().data.numpy() * bs
 
         if is_train <= 1:  # if is validation or train simply output the overall mean error
-            mpjpe_p3d_h36 = torch.mean(torch.norm(p3d_h36[:, in_n:in_n + out_n] - p3d_out, dim=3))
-            m_p3d_h36 += mpjpe_p3d_h36.cpu().data.numpy() * batch_size
+            mpjpe_p3d_h36 = torch.mean(torch.norm(data[:, in_n:in_n + out_n] - p3d_out, dim=3))
+            m_p3d_h36 += mpjpe_p3d_h36.cpu().data.numpy() * bs
         else:
-            mpjpe_p3d_h36 = torch.sum(torch.mean(torch.norm(p3d_h36[:, in_n:] - p3d_out, dim=3), dim=2), dim=0)
+            mpjpe_p3d_h36 = torch.sum(torch.mean(torch.norm(data[:, in_n:] - p3d_out, dim=3), dim=2), dim=0)
             m_p3d_h36 += mpjpe_p3d_h36.cpu().data.numpy()
-        if i % 1000 == 0:
-            print('{}/{}|bt {:.3f}s|tt{:.0f}s|gn{}'.format(i + 1, len(data_loader), time.time() - bt,
-                                                           time.time() - st, grad_norm))
-    ret = {}
-    if is_train == 0:
-        ret["l_p3d"] = l_p3d / n
 
-    if is_train <= 1:
-        ret["m_p3d_h36"] = m_p3d_h36 / n
-    else:
-        m_p3d_h36 = m_p3d_h36 / n
-        for j in range(out_n):
-            ret["#{:d}".format(titles[j])] = m_p3d_h36[j]
     return ret
