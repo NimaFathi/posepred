3d3b2766e5b7f70197594046ce51c6d88a95ad6e
rmool <armin2saadat@gmail.com>
2021-09-20 01:37:25 +0430

    debug

--- models/temp.py
+++ models/temp.py
@@ -1,25 +1,17 @@
-import torch
+for i, data in enumerate(data_loader):
+    bs, in_out_sum, feature_n = data.shape
 
+    sup_seq = data.clone()[:, -(kernel_size + out_n):, :].reshape([bs, kernel_size + out_n, len(feature_n) // 3, 3])
+    src = data.clone()
 
-def run_model(net_pred):
-    in_n = input_n
-    out_n = output_n
-    seq_in = kernel_size
+    out_all = net_pred(src, input_n=in_n, output_n=out_n)
+    # out.shape: [bs, seq_in + out_n, itera, feature_n]
 
+    out = out_all[:, kernel_size:, 0]
+    out = out.reshape([bs, out_n, len(feature_n) // 3, 3])
+    data = data.reshape([bs, in_n + out_n, len(feature_n) // 3, 3])
+    out_all = out_all.reshape([bs, kernel_size + out_n, itera, len(feature_n) // 3, 3])
 
-    for i, data in enumerate(data_loader):
-        bs, in_out_sum, feature_n = data.shape
+    loss_p3d = torch.mean(torch.norm(out_all[:, :, 0] - sup_seq, dim=3))
 
-        sup_seq = data.clone()[:, -(seq_in + out_n):, :].reshape([bs, seq_in + out_n, len(feature_n) // 3, 3])
-        src = data.clone()
-
-        out_all = net_pred(src, input_n=in_n, output_n=out_n)
-
-        out = out_all[:, seq_in:, 0]
-        out = out.reshape([bs, out_n, len(feature_n) // 3, 3])
-        data = data.reshape([bs, in_n + out_n, len(feature_n) // 3, 3])
-        out_all = out_all.reshape([bs, seq_in + out_n, itera, len(feature_n) // 3, 3])
-
-        loss_p3d = torch.mean(torch.norm(out_all[:, :, 0] - sup_seq, dim=3))
-
-        mpjpe_p3d_h36 = torch.mean(torch.norm(data[:, in_n:] - out, dim=3))
+    mpjpe_p3d_h36 = torch.mean(torch.norm(data[:, in_n:] - out, dim=3))
