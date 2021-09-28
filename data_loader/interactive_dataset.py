bf6e5abb234a343ce1d3eb295916535874b2a1b8
rmool <armin2saadat@gmail.com>
2021-09-28 17:29:20 +0330

    change output type to dict in interactive_dataset.

--- data_loader/interactive_dataset.py
+++ data_loader/interactive_dataset.py
@@ -31,7 +31,7 @@ class InteractiveDataset(Dataset):
         self.skip_frame = skip_frame
         self.is_visualizing = is_visualizing
 
-        seq = self.data.iloc[0] #[1:].apply(lambda x: literal_eval(x))
+        seq = self.data.iloc[0]  # [1:].apply(lambda x: literal_eval(x))
         self.keypoint_dim = keypoint_dim
         self.keypoints_num = len(seq.observed_pose[0][0]) / self.keypoint_dim
         self.obs_frames_num = len(seq.observed_pose[0])
@@ -44,7 +44,7 @@ class InteractiveDataset(Dataset):
     def __getitem__(self, index):
 
         try:
-            seq = self.data.iloc[index] #[1:].apply(lambda x: literal_eval(x))
+            seq = self.data.iloc[index]  # [1:].apply(lambda x: literal_eval(x))
         except Exception:
             msg = "Each row must be convertible to python list"
             logger.exception(msg=msg)
@@ -53,15 +53,15 @@ class InteractiveDataset(Dataset):
         persons_in_seq = self.select_persons(seq)
 
         try:
-            obs_pose = self.get_tensor(seq, 'observed_pose', persons_in_seq, self.obs_frames_num)
-            outputs = {'obs_pose': obs_pose}
+            observed_pose = self.get_tensor(seq, 'observed_pose', persons_in_seq, self.obs_frames_num)
+            outputs = {'observed_pose': observed_pose}
         except:
             logger.warning('faulty row skipped.')
             return self.__getitem__((index + 1) % self.__len__())
 
         if self.use_mask:
-            obs_mask = self.get_tensor(seq, 'observed_mask', persons_in_seq, self.obs_frames_num)
-            outputs['obs_mask'] = obs_mask
+            observed_mask = self.get_tensor(seq, 'observed_mask', persons_in_seq, self.obs_frames_num)
+            outputs['observed_mask'] = observed_mask
 
         if not self.is_testing:
             assert len(seq.observed_pose) == len(seq.future_pose), "unequal persons in observed and future frames."
@@ -70,23 +70,21 @@ class InteractiveDataset(Dataset):
 
             if self.use_mask:
                 future_mask = self.get_tensor(seq, 'future_mask', persons_in_seq, self.future_frames_num)
-                outputs.append(future_mask)
-                outputs_vis['future_mask'] = future_mask
+                outputs['future_mask'] = future_mask
 
         if self.is_visualizing:
             if 'observed_image_path' in seq.keys():
-                outputs_vis['obs_image'] = seq['observed_image_path']
+                outputs['observed_image'] = seq['observed_image_path']
             if 'future_image_path' in seq.keys():
-                outputs_vis['future_image'] = seq['future_image_path']
+                outputs['future_image'] = seq['future_image_path']
             if 'observed_cam_extrinsic' in seq.keys():
-                outputs_vis['obs_cam_ex'] = torch.tensor(seq['observed_cam_extrinsic'])
+                outputs['observed_cam_ex'] = torch.tensor(seq['observed_cam_extrinsic'])
             if 'future_cam_extrinsic' in seq.keys():
-                outputs_vis['future_cam_ex'] = torch.tensor(seq['future_cam_extrinsic'])
+                outputs['future_cam_ex'] = torch.tensor(seq['future_cam_extrinsic'])
             if 'cam_intrinsic' in seq.keys():
-                outputs_vis['cam_in'] = torch.tensor(seq['cam_intrinsic'])
-            return outputs_vis
+                outputs['cam_in'] = torch.tensor(seq['cam_intrinsic'])
 
-        return tuple(outputs)
+        return outputs
 
     def select_persons(self, seq):
         persons_in_seq = list(range(len(seq['observed_pose'])))
