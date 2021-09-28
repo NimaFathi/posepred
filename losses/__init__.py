6edf048ee70b4d10094c0e2c7dc06a33b3290e65
rmool <armin2saadat@gmail.com>
2021-09-28 23:15:31 +0330

    refactor.

--- losses/__init__.py
+++ losses/__init__.py
@@ -1,5 +1,5 @@
 from .mse_vel_loss import MSEVelLoss
 from .mae_vel_loss import MAEVelLoss
 
-LOSSES = {'mse': MSEVelLoss,
-          'mae': MAEVelLoss}
+LOSSES = {'mse_vel_loss': MSEVelLoss,
+          'mae_vel_loss': MAEVelLoss}
