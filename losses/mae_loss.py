a58bfbaf277117ed9ade488d2bd4c93d11a0dfa5
rmool <armin2saadat@gmail.com>
2021-09-28 23:13:46 +0330

    implement MAEVelLoss.

--- losses/mae_loss.py
+++ losses/mae_loss.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 
@@ -6,7 +7,21 @@ class MAELoss(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.loss = nn.L1Loss()
+        self.mae = nn.MSELoss()
+        self.bce = nn.BCELoss()
 
-    def forward(self, pred, target):
-        return self.loss(pred, target)
+    def forward(self, model_outputs, input_data):
+        observed_pose = input_data['observed_pose']
+        future_pose = input_data['future_pose']
+        future_vel = torch.cat(((future_pose[..., 0, :] - observed_pose[..., -1, :]).unsqueeze(-2),
+                                future_pose[..., 1:, :] - future_pose[..., :-1, :]), -2)
+        vel_loss = self.mae(model_outputs['pred_vel'], future_vel)
+
+        if 'pred_mask' in model_outputs.keys():
+            mask_loss = self.bce(model_outputs['pred_mask'], input_data['future_mask'])
+        else:
+            mask_loss = 0
+
+        loss = vel_loss + (0.3 * mask_loss)
+
+        return loss
