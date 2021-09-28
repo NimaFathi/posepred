d98d30fd087720406e3351746d9f62d52514ac3e
rmool <armin2saadat@gmail.com>
2021-09-28 16:38:58 +0330

    refactor

--- factory/trainer.py
+++ factory/trainer.py
@@ -40,11 +40,8 @@ class Trainer:
         for epoch in range(self.args.start_epoch, self.args.epochs):
             self.__train()
             if self.use_validation:
-                logger.info("USE VALIDATION")
                 self.__validate()
                 self.scheduler.step(self.valid_reporter.history['vel_loss'][-1])
-            else:
-                logger.info("DO NOT USE VALIDATION")
             if (epoch + 1) % self.args.snapshot_interval == 0 or (epoch + 1) == self.args.epochs:
                 save_snapshot(self.model, self.optimizer, self.args.lr, epoch + 1, self.train_reporter,
                               self.valid_reporter, self.args.save_dir)
