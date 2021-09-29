94f93ab384fb1318cbb18797d9ed720120e2cb30
rmool <armin2saadat@gmail.com>
2021-09-29 03:52:04 +0330

    fix predictor

--- factory/predictor.py
+++ factory/predictor.py
@@ -6,7 +6,6 @@ import pandas as pd
 import torch
 
 from path_definition import LOGGER_CONF
-from utils.others import pose_from_vel
 from utils.save_load import save_test_results
 
 config.fileConfig(LOGGER_CONF)
@@ -23,11 +22,10 @@ class Predictor:
 
         self.result = pd.DataFrame()
         self.pred_pose = torch.Tensor().to('cuda')
-        self.pred_vel = torch.Tensor().to('cuda')
         self.pred_mask = torch.Tensor().to('cuda')
 
     def predict(self):
-        logger.info("Prediction started ...")
+        logger.info("Prediction started.")
         self.model.eval()
         time0 = time.time()
         self.__predict()
@@ -37,24 +35,26 @@ class Predictor:
 
     def __predict(self):
         for data in self.dataloader:
-            for i, d in enumerate(data):
-                data[i] = d.to(self.device)
-            if self.model.args.use_mask:
-                obs_pose, obs_vel, obs_mask = data
-            else:
-                obs_pose, obs_vel = data
+            for key, value in data.items():
+                data[key] = value.to(self.device)
 
             with torch.no_grad():
-                outputs = self.model(data)
-                pred_vel = outputs[0]
-                pred_pose = pose_from_vel(pred_vel, obs_pose[..., -1, :])
-                pred_mask = outputs[1] if self.model.args.use_mask else None
-                self.store_results(pred_pose, pred_vel, pred_mask)
+                # predict & calculate loss
+                model_outputs = self.model(data)
+                assert 'pred_pose' in model_outputs.keys(), 'outputs of model should include pred_pose'
+                pred_pose = model_outputs['pred_pose']
 
-    def store_results(self, pred_pose, pred_vel, pred_mask):
+                if self.model.args.use_mask:
+                    assert 'pred_mask' in model_outputs.keys(), 'outputs of model should include pred_mask'
+                    pred_mask = model_outputs['pred_mask']
+                else:
+                    pred_mask = None
+
+                self.store_results(pred_pose, pred_mask)
+
+    def store_results(self, pred_pose, pred_mask):
         # update tensors
         self.pred_pose = torch.cat((self.pred_pose, pred_pose), 0)
-        self.pred_vel = torch.cat((self.pred_vel, pred_vel), 0)
         if self.model.args.use_mask:
             self.pred_mask = torch.cat((self.pred_mask, pred_mask), 0)
 
@@ -62,9 +62,7 @@ class Predictor:
         for i in range(pred_pose.shape[0]):
             if self.model.args.use_mask:
                 single_data = {'pred_pose': str(pred_pose[i].detach().cpu().numpy().tolist()),
-                               'pred_vel': str(pred_vel[i].detach().cpu().numpy().tolist()),
                                'pred_mask': str(pred_mask[i].detach().cpu().numpy().round().tolist())}
             else:
-                single_data = {'pred_pose': str(pred_pose[i].detach().cpu().numpy().tolist()),
-                               'pred_vel': str(pred_vel[i].detach().cpu().numpy().tolist())}
+                single_data = {'pred_pose': str(pred_pose[i].detach().cpu().numpy().tolist()), }
             self.result = self.result.append(single_data, ignore_index=True)
