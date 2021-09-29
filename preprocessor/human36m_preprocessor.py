e579dd27719d446d5670c725671ff15bebc47d04
Nima Fathi <nimafathinimafathi@gmail.com>
2021-09-29 05:53:18 +0330

    Fix: Fix consoleLogger in configs

--- preprocessor/human36m_preprocessor.py
+++ preprocessor/human36m_preprocessor.py
@@ -1,12 +1,17 @@
+import logging
 import os
 from glob import glob
+from logging import config
 
 import cdflib
 import jsonlines
 import numpy as np
 
-from path_definition import PREPROCESSED_DATA_DIR
+from path_definition import PREPROCESSED_DATA_DIR, LOGGER_CONF
 from preprocessor.preprocessor import Processor
+
+config.fileConfig(LOGGER_CONF)
+logger = logging.getLogger('consoleLogger')
 
 
 class PreprocessorHuman36m(Processor):
@@ -30,13 +35,14 @@ class PreprocessorHuman36m(Processor):
         self.subjects = ['S1', 'S5', 'S6', 'S7', 'S8', 'S9', 'S11']
 
     def normal(self, data_type='train'):
-        print('start creating Human3.6m normal static data from origian Human3.6m dataset (CDF files) ... ')
+        logger.info('start creating Human3.6m normal static data from original Human3.6m dataset (CDF files) ... ')
         if self.custom_name:
             output_file_name = f'{data_type}_{self.obs_frame_num}_{self.pred_frame_num}_{self.skip_frame_num}_{self.custom_name}.jsonl'
         else:
             output_file_name = f'{data_type}_{self.obs_frame_num}_{self.pred_frame_num}_{self.skip_frame_num}_human3.6m.jsonl'
         assert os.path.exists(os.path.join(self.output_dir, output_file_name)) is False, "preprocessed file exists"
         for subject in self.subjects:
+            logger.info("handling subject: {}".format(subject))
             file_list = glob(self.dataset_path + '/' + subject + '/MyPoseFeatures/D3_Positions/*.cdf')
             assert len(file_list) == 30, "Expected 30 files for subject " + subject + ", got " + str(len(file_list))
             for f in file_list:
@@ -52,13 +58,13 @@ class PreprocessorHuman36m(Processor):
                 positions /= 1000
                 total_frame_num = self.obs_frame_num + self.pred_frame_num
                 section_range = positions.shape[0] // (
-                            total_frame_num * (self.skip_frame_num + 1)) if self.use_video_once is False else 1
+                        total_frame_num * (self.skip_frame_num + 1)) if self.use_video_once is False else 1
                 for i in range(section_range):
                     video_data = {
                         'obs_pose': list(),
                         'future_pose': list(),
                     }
-                    for j in range(1, total_frame_num * (self.skip_frame_num + 1) + 1, self.skip_frame_num + 1):
+                    for j in range(0, total_frame_num * (self.skip_frame_num + 1), self.skip_frame_num + 1):
                         if j <= (self.skip_frame_num + 1) * self.obs_frame_num:
                             video_data['obs_pose'].append(
                                 positions[i * total_frame_num * (self.skip_frame_num + 1) + j].tolist())
