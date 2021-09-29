bc02ca379aef19f1e5bb84992c259a6368e862c2
rmool <armin2saadat@gmail.com>
2021-09-29 05:49:56 +0330

    fix nearest_neighbor

--- models/nearest_neighbor.py
+++ models/nearest_neighbor.py
@@ -1,6 +1,8 @@
 import torch
 from torch import nn
 
+from utils.others import pose_from_vel
+
 
 class NearestNeighbor(nn.Module):
     def __init__(self, args):
@@ -14,7 +16,8 @@ class NearestNeighbor(nn.Module):
         best_pred_vel = None
         best_pred_mask = None
 
-        in_vel = inputs[1].view(1, -1)
+        obs_pose = inputs['observed_pose']
+        in_vel = (obs_pose[..., 1:, :] - obs_pose[..., :-1, :]).view(1, -1)
         assert in_vel.shape[0] == 1, "only support batch_size 1 in nearest neighbor"
 
         for data in self.train_dataloader:
@@ -30,9 +33,12 @@ class NearestNeighbor(nn.Module):
                 else:
                     best_pred_vel = data[3][ind]
 
-        if self.args.use_mask:
-            outputs = [best_pred_vel.unsqueeze(0).to('cuda'), best_pred_mask.unsqueeze(0).to('cuda')]
-        else:
-            outputs = [best_pred_vel.unsqueeze(0).to('cuda')]
+        pred_vel = best_pred_vel.unsqueeze(0).to('cuda')
+        pred_pose = pose_from_vel(pred_vel, obs_pose[..., -1, :])
+        outputs = {'pred_pose': pred_pose, 'pred_vel': pred_vel}
 
-        return tuple(outputs)
+        if self.args.use_mask:
+            pred_mask = best_pred_mask.unsqueeze(0).to('cuda')
+            outputs['pred_mask'] = pred_mask
+
+        return outputs
