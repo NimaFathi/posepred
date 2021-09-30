b55f58d0d8370a910064a5a3a2fbfeaa0f8065a2
rmool <armin2saadat@gmail.com>
2021-09-30 17:58:54 +0330

    fix jsonlines in interactive_dataset

--- data_loader/interactive_dataset.py
+++ data_loader/interactive_dataset.py
@@ -1,8 +1,7 @@
 import torch
 from torch.utils.data import Dataset
 from numpy import random
-import pandas as pd
-from ast import literal_eval
+import jsonlines
 import logging
 
 logger = logging.getLogger(__name__)
@@ -10,55 +9,57 @@ logger = logging.getLogger(__name__)
 
 class InteractiveDataset(Dataset):
     def __init__(self, dataset_path, keypoint_dim, persons_num, is_testing, use_mask, skip_frame, is_visualizing):
-        data = pd.read_csv(dataset_path)
 
-        for col in list(data.columns[1:].values):
-            try:
-                data.loc[:, col] = data.loc[:, col].apply(lambda x: literal_eval(x))
-            except Exception:
-                msg = "Each row must be convertible to python list"
-                logger.exception(msg=msg)
-                raise Exception(msg)
+        tensor_keys = ['observed_pose', 'future_pose', 'observed_mask', 'future_mask']
+        data = list()
+        with jsonlines.open(dataset_path) as reader:
+            for seq in reader:
+                seq_tensor = {}
+                for k, v in seq.items():
+                    if k in tensor_keys:
+                        seq_tensor[k] = torch.tensor(v, dtype=torch.float32)
+                    else:
+                        seq_tensor[k] = v
+                data.append(seq_tensor)
 
-        self.data = data.copy().reset_index(drop=True)
+        self.data = data
+        self.keypoint_dim = keypoint_dim
         self.persons_num = persons_num
         self.is_testing = is_testing
         self.use_mask = use_mask
         self.skip_frame = skip_frame
         self.is_visualizing = is_visualizing
 
-        seq = self.data.iloc[0]  # [1:].apply(lambda x: literal_eval(x))
-        self.keypoint_dim = keypoint_dim
-        self.keypoints_num = len(seq.observed_pose[0][0]) / self.keypoint_dim
-        self.obs_frames_num = len(seq.observed_pose[0])
+        seq = self.data[0]
+        assert 'observed_pose' in seq.keys(), 'dataset must include observed_pose'
+        self.keypoints_num = int(seq['observed_pose'].shape[-1] / self.keypoint_dim)
+        self.obs_frames_num = seq['observed_pose'].shape[-2]
         if not self.is_testing:
-            self.future_frames_num = len(seq.future_pose[0])
+            assert 'future_pose' in seq.keys(), 'dataset must include future_pose'
+            self.future_frames_num = seq['future_pose'].shape[-2]
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, index):
-        seq = self.data.iloc[index]  # [1:].apply(lambda x: literal_eval(x))
+        seq = self.data[index]
         persons_in_seq = self.select_persons(seq)
 
-        try:
-            observed_pose = self.get_tensor(seq, 'observed_pose', persons_in_seq, self.obs_frames_num)
-            outputs = {'observed_pose': observed_pose}
-        except:
-            logger.warning('faulty row skipped.')
-            return self.__getitem__((index + 1) % self.__len__())
+        observed_pose = self.fix_persons(seq, 'observed_pose', persons_in_seq)
+        outputs = {'observed_pose': observed_pose}
 
         if self.use_mask:
-            observed_mask = self.get_tensor(seq, 'observed_mask', persons_in_seq, self.obs_frames_num)
+            observed_mask = self.fix_persons(seq, 'observed_mask', persons_in_seq)
             outputs['observed_mask'] = observed_mask
 
         if not self.is_testing:
-            assert len(seq.observed_pose) == len(seq.future_pose), "unequal persons in observed and future frames."
-            future_pose = self.get_tensor(seq, 'future_pose', persons_in_seq, self.future_frames_num)
+            assert seq['observed_pose'].shape[0] == seq['future_pose'].shape[
+                0], "unequal number of persons in observed and future frames."
+            future_pose = self.fix_persons(seq, 'future_pose', persons_in_seq)
             outputs['future_pose'] = future_pose
 
             if self.use_mask:
-                future_mask = self.get_tensor(seq, 'future_mask', persons_in_seq, self.future_frames_num)
+                future_mask = self.fix_persons(seq, 'future_mask', persons_in_seq)
                 outputs['future_mask'] = future_mask
 
         if self.is_visualizing:
@@ -76,19 +77,15 @@ class InteractiveDataset(Dataset):
         return outputs
 
     def select_persons(self, seq):
-        persons_in_seq = list(range(len(seq['observed_pose'])))
+        persons_in_seq = list(range(seq['observed_pose'].shape[0]))
         if self.persons_num < len(persons_in_seq):
             return random.choice(persons_in_seq, self.persons_num, replace=False)
         else:
             return persons_in_seq
 
-    def get_tensor(self, seq, segment, persons_in_seq, frames_num):
+    def fix_persons(self, seq, segment, persons_in_seq):
         assert segment in seq, 'No segment named: ' + segment
-        result = torch.tensor(
-            [[seq[segment][person_idx][frame_idx]
-              for frame_idx in range(0, frames_num, self.skip_frame + 1)]
-             for person_idx in persons_in_seq], dtype=torch.float32)
-
+        result = seq[segment][persons_in_seq]
         if len(persons_in_seq) < self.persons_num:
             padding = torch.zeros(self.persons_num - len(persons_in_seq), result.shape[1], result.shape[2])
             result = torch.cat((result, padding), dim=0)
