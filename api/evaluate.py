253f9fd991955625cc310ed959ffbf8c7594c63a
rmool <armin2saadat@gmail.com>
2021-10-02 02:42:01 +0330

    debug and fix nearest neighbor.

--- api/evaluate.py
+++ api/evaluate.py
@@ -6,9 +6,10 @@ from data_loader.my_dataloader import get_dataloader
 from models import MODELS
 from losses import LOSSES
 from factory.evaluator import Evaluator
-from path_definition import HYDRA_PATH
 from utils.reporter import Reporter
 from utils.save_load import load_snapshot
+
+from path_definition import HYDRA_PATH
 
 logger = logging.getLogger(__name__)
 
@@ -19,25 +20,21 @@ def evaluate(cfg: DictConfig):
         msg = 'either specify a load_path or config a model.'
         logger.error(msg)
         raise Exception(msg)
-    if cfg.train_dataset is not None:
-        pass
-
-    eval_dataloader = get_dataloader(cfg.eval_dataset, cfg.data)
-    eval_reporter = Reporter(state='eval')
+    dataloader = get_dataloader(cfg.dataset, cfg.data)
+    eval_reporter = Reporter(state='')
     if cfg.load_path is not None:
         model, loss_module, _, _, _, _, _ = load_snapshot(cfg.load_path)
     else:
         cfg.model.keypoint_dim = cfg.data.keypoint_dim
-        cfg.model.pred_frames_num = eval_dataloader.dataset.future_frames_num
-        cfg.model.keypoints_num = eval_dataloader.dataset.keypoints_num
+        cfg.model.pred_frames_num = dataloader.dataset.future_frames_num
+        cfg.model.keypoints_num = dataloader.dataset.keypoints_num
         cfg.model.use_mask = cfg.data.use_mask
         model = MODELS[cfg.model.type](cfg.model)
         loss_module = LOSSES[cfg.loss.type](cfg.loss)
         if cfg.model.type == 'nearest_neighbor':
-            assert cfg.train_dataset is not None, 'Please provide a train_dataset for nearest_neighbor model.'
-            model.train_dataloader = get_dataloader(cfg.train_dataset, cfg.data)
+            model.train_dataloader = get_dataloader(cfg.model.train_dataset, cfg.data)
 
-    evaluator = Evaluator(cfg, eval_dataloader, model, loss_module, eval_reporter)
+    evaluator = Evaluator(cfg, dataloader, model, loss_module, eval_reporter)
     evaluator.evaluate()
 
 
--- api/generate_final_output.py
+++ api/generate_final_output.py
@@ -1,39 +1,32 @@
 import logging
-
 import hydra
 from omegaconf import DictConfig
 
-from configs.helper import DataloaderArgs, ModelArgs
 from data_loader.my_dataloader import get_dataloader
+from models import MODELS
 from factory.output_generator import Output_Generator
-from models import get_model
+from utils.save_load import load_snapshot, setup_testing_dir
+
 from path_definition import HYDRA_PATH
 from path_definition import ROOT_DIR
-from utils.save_load import load_snapshot, save_args, setup_testing_dir
 
 logger = logging.getLogger(__name__)
 
 
 @hydra.main(config_path=HYDRA_PATH, config_name="predict")
 def generate_output(cfg: DictConfig):
-    dataloader_args = DataloaderArgs(cfg.dataloader.dataset_file_name, cfg.keypoint_dim, cfg.interactive,
-                                     cfg.persons_num, cfg.use_mask, cfg.skip_num, cfg.dataloader.batch_size,
-                                     cfg.dataloader.shuffle, cfg.pin_memory, cfg.num_workers, is_testing=True)
-    model_args = ModelArgs(cfg.model.model_name, cfg.use_mask, cfg.keypoint_dim)
+    if cfg.load_path is None and cfg.model is None:
+        msg = 'either specify a load_path or config a model.'
+        logger.error(msg)
+        raise Exception(msg)
 
-    if cfg.train_dataset is not None:
-        train_dataloader_args = DataloaderArgs(cfg.train_dataset, cfg.keypoint_dim, cfg.interactive,
-                                               cfg.persons_num, cfg.use_mask, cfg.skip_num, 1, False,
-                                               cfg.pin_memory, cfg.num_workers)
+    dataloader = get_dataloader(cfg.test_dataset, cfg.data)
+
+    if cfg.load_path is not None:
+        model, _, _, _, _, _, _ = load_snapshot(cfg.load_path)
     else:
-        train_dataloader_args = None
-    dataloader = get_dataloader(dataloader_args)
-
-    if cfg.load_path:
-        model, _, _, _, _ = load_snapshot(cfg.load_path)
-    elif model_args.model_name:
-        if model_args.model_name == 'nearest_neighbor':
-            assert train_dataloader_args is not None, 'Please provide a train_dataset for nearest_neighbor model.'
+        if cfg.model.type == 'nearest_neighbor':
+            assert cfg.train_dataset is not None, 'Please provide a train_dataset for nearest_neighbor model.'
             train_dataloader = get_dataloader(train_dataloader_args)
             model_args.pred_frames_num = train_dataloader.dataset.future_frames_num
             model_args.keypoints_num = dataloader.dataset.keypoints_num
@@ -44,10 +37,6 @@ def generate_output(cfg: DictConfig):
             assert model_args.pred_frames_num is not None, 'specify pred_frames_num'
             model_args.keypoints_num = dataloader.dataset.keypoints_num
             model = get_model(model_args).to('cuda')
-    else:
-        msg = "Please provide either a model_name or a load_path to a trained model."
-        logger.error(msg)
-        raise Exception(msg)
 
     save_dir = setup_testing_dir(ROOT_DIR)
     save_args({'dataloader_args': dataloader_args, 'model_args': model.args}, save_dir)
