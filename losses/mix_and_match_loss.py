5ab6125bdded7202ff5a1970777dcd3627fcd047
Nima Fathi <nimafathinimafathi@gmail.com>
2021-10-11 03:46:28 +0330

    Update: update mix_and_match_loss.py loss to use quaternion data

--- losses/mix_and_match_loss.py
+++ losses/mix_and_match_loss.py
@@ -1,10 +1,8 @@
-import torch.nn as nn
-import numpy as np
-from losses import KLDivergenceLoss
 import numpy as np
 import torch.nn as nn
 
 from losses import KLDivergenceLoss
+from utils.others import qeuler
 
 
 class MixAndMatchLoss(nn.Module):
@@ -17,16 +15,20 @@ class MixAndMatchLoss(nn.Module):
 
         self.args = args
         self.kl_loss = KLDivergenceLoss(self.args)
+        self.L_rot = nn.MSELoss()
         self.L_skl = nn.MSELoss()
         self.L_rot = nn.MSELoss()
         self.step = 0
 
     def forward(self, model_outputs, input_data=None):
+        q_target_pose = qeuler(q=input_data['future_pose'], order='xyz')
+        rot_loss = self.L_rot(model_outputs['pred_q_pose'], q_target_pose)
         pose_loss = self.L_skl(model_outputs['pred_pose'], input_data['future_pose'])
         kl_loss = self.kl_loss(model_outputs)
         kl_weight = self.kl_anneal_function(self.args.anneal_function)
-        loss = pose_loss + kl_weight * kl_loss
-        outputs = {'loss': loss, 'L_skl': pose_loss, 'L_prior': kl_weight * kl_loss, 'kl_weight': kl_weight}
+        loss = rot_loss + pose_loss + kl_weight * kl_loss
+        outputs = {'loss': loss, 'L_rot': rot_loss, 'L_skl': pose_loss, 'L_prior': kl_weight * kl_loss,
+                   'kl_weight': kl_weight}
         return outputs
 
     def kl_anneal_function(self, anneal_function):
