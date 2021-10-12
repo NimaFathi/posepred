87af96e60a4980d02eebaeae4bea8ae365cb0ba1
Nima Fathi <nimafathinimafathi@gmail.com>
2021-10-12 05:55:13 +0330

    Update: update kl_divergence.py

--- losses/kl_divergence.py
+++ losses/kl_divergence.py
@@ -11,6 +11,6 @@ class KLDivergenceLoss(nn.Module):
 
     def forward(self, model_outputs, input_data=None):
         kl_loss = -0.5 * torch.sum(
-            1 + model_outputs['sigma'] - model_outputs['mu'].pow(2) - model_outputs['sigma'].exp())
+            1 + model_outputs['sigma'] - model_outputs['mu'].pow(2) - model_outputs['sigma'].exp(), dim=1)
         outputs = {'loss': kl_loss}
         return outputs
