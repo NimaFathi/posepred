d3bdad72f089b14a68a473d5f7f77e4b915cd77e
rmool <armin2saadat@gmail.com>
2021-10-13 14:05:11 +0330

    add HisRepItselfLoss to init.

--- losses/__init__.py
+++ losses/__init__.py
@@ -4,11 +4,13 @@ from .kl_divergence import KLDivergenceLoss
 from .mix_and_match_loss import MixAndMatchLoss
 from .elbo import ELBO
 from .derpof_loss import DeRPoFLoss
+from .his_rep_itself_loss import HisRepItselfLoss
 
 LOSSES = {'mse_vel': MSEVel,
           'mae_vel': MAEVel,
           'kl_loss': KLDivergenceLoss,
           'm&m': MixAndMatchLoss,
           'elbo': ELBO,
-          'derpof': DeRPoFLoss
+          'derpof': DeRPoFLoss,
+          'his_rep_itself': HisRepItselfLoss,
           }
