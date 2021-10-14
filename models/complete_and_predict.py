b3d5f552f6a0b843f9e6d1efc1be76c64ac9665d
rmool <armin2saadat@gmail.com>
2021-10-14 23:33:28 +0330

    finish comp&pred model.

--- models/complete_and_predict.py
+++ models/complete_and_predict.py
@@ -29,32 +29,43 @@ class CompleteAndPredict(nn.Module):
         self.vel_decoder = Decoder(args.pred_frames_num, input_size, output_size, args.hidden_size, args.n_layers,
                                    args.dropout_pose_dec, 'hardtanh', args.hardtanh_limit)
 
+        self.completion = Completion(input_size, output_size, args.hidden_size, args.n_layers, args.dropout_pose_dec,
+                                     'hardtanh', args.hardtanh_limit)
+
     def forward(self, inputs):
         pose = inputs['observed_pose']
         vel = pose[..., 1:, :] - pose[..., :-1, :]
         mask = inputs['observed_mask'][..., 1:, :]
 
+        # make data noisy
         bs, frames_n, features_n = vel.shape
         vel = vel.reshape(bs, frames_n, self.args.keypoints_num, self.args.keypoint_dim)
-
         mask = mask.repeat(1, 1, 1, 3)
-        const = torch.zeros_like(mask) * -1000
-
+        const = torch.zeros_like(mask) * (-100)
         noisy_vel = torch.where(mask == 1, const, vel).reshape(bs, frames_n, -1)
 
+        # velocity encoder
         (hidden_vel, cell_vel) = self.vel_encoder(noisy_vel.permute(1, 0, 2))
         hidden_vel = hidden_vel.squeeze(0)
         cell_vel = cell_vel.squeeze(0)
 
+        # VAE encoder
         fusion1 = self.res_block1(hidden_vel, nn.ReLU())
         latent = self.reparameterize(self.mean(fusion1), self.std(fusion1))
+
+        # VAE decoder
         fusion2 = self.decode_latent(latent)
         hidden_vel2 = self.res_block2(fusion2, nn.ReLU())
 
-        vel_dec_input = vel[:, -1, :]
+        # velocity decoder
+        vel_dec_input = noisy_vel[..., -1, :]
         pred_vel = self.vel_decoder(vel_dec_input, hidden_vel2, cell_vel)
         pred_pose = pose_from_vel(pred_vel, pose[..., -1, :])
-        outputs = {'pred_pose': pred_pose, 'pred_vel': pred_vel}
+
+        # completion
+        complited_vel = self.completion(noisy_vel, hidden_vel2, cell_vel)
+
+        outputs = {'pred_pose': pred_pose, 'pred_vel': pred_vel, 'completed_vel': complited_vel}
 
         return outputs
 
@@ -95,8 +106,7 @@ class Decoder(nn.Module):
         if len(hiddens.shape) < 3 or len(cells.shape) < 3:
             hiddens = torch.unsqueeze(hiddens, 0)
             cells = torch.unsqueeze(cells, 0)
-        device = 'cuda' if inputs.is_cuda else 'cpu'
-        outputs = torch.tensor([], device=device)
+        outputs = torch.tensor([], device=self.args.device)
         for j in range(self.outputs_num):
             for i, lstm in enumerate(self.lstms):
                 if i == 0:
@@ -105,6 +115,39 @@ class Decoder(nn.Module):
                     hiddens[i], cells[i] = lstm(hiddens.clone()[i - 1], (hiddens.clone()[i], cells.clone()[i]))
             output = self.activation(self.fc_out(hiddens.clone()[-1]))
             dec_inputs = output.detach()
+            outputs = torch.cat((outputs, output.unsqueeze(1)), 1)
+        return outputs
+
+
+class Completion(nn.Module):
+    def __init__(self, input_size, output_size, hidden_size, n_layers, dropout, activation_type,
+                 hardtanh_limit=None):
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+        lstms = [
+            nn.LSTMCell(input_size=input_size if i == 0 else hidden_size, hidden_size=hidden_size) for
+            i in range(n_layers)]
+        self.lstms = nn.Sequential(*lstms)
+        self.fc_out = nn.Linear(in_features=hidden_size, out_features=output_size)
+        if activation_type == 'hardtanh':
+            self.activation = nn.Hardtanh(min_val=-1 * hardtanh_limit, max_val=hardtanh_limit, inplace=False)
+        else:
+            self.activation = nn.Sigmoid()
+
+    def forward(self, inputs, hiddens, cells):
+        frames_n = inputs.shape[-2]
+        dec_inputs = self.dropout(inputs)
+        if len(hiddens.shape) < 3 or len(cells.shape) < 3:
+            hiddens = torch.unsqueeze(hiddens, 0)
+            cells = torch.unsqueeze(cells, 0)
+        outputs = torch.tensor([], device=self.args.device)
+        for j in range(frames_n):
+            for i, lstm in enumerate(self.lstms):
+                if i == 0:
+                    hiddens[i], cells[i] = lstm(dec_inputs[..., j, :], (hiddens.clone()[i], cells.clone()[i]))
+                else:
+                    hiddens[i], cells[i] = lstm(hiddens.clone()[i - 1], (hiddens.clone()[i], cells.clone()[i]))
+            output = self.activation(self.fc_out(hiddens.clone()[-1]))
             outputs = torch.cat((outputs, output.unsqueeze(1)), 1)
         return outputs
 
