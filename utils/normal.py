c177d803bb523c4456e71ff16baddbb269314e69
rmool <armin2saadat@gmail.com>
2021-11-01 03:25:41 +0330

    make std and mean torch in normal file.

--- utils/normal.py
+++ utils/normal.py
@@ -1,6 +1,11 @@
+import torch
+
+
 def normalize(in_tensor, mean, std):
     bs, frame_n, feature_n = in_tensor.shape
     keypoint_dim = mean.shape[0]
+    mean = torch.tensor(mean)
+    std = torch.tensor(std)
     assert mean.shape == std.shape
     assert feature_n % keypoint_dim == 0
     mean = mean.unsqueeze(0).repeat(bs, frame_n, feature_n // keypoint_dim)
@@ -12,6 +17,8 @@ def normalize(in_tensor, mean, std):
 def denormalize(in_tensor, mean, std):
     bs, frame_n, feature_n = in_tensor.shape
     keypoint_dim = mean.shape[0]
+    mean = torch.tensor(mean)
+    std = torch.tensor(std)
     assert mean.shape == std.shape
     assert feature_n % keypoint_dim == 0
     mean = mean.unsqueeze(0).repeat(bs, frame_n, feature_n // keypoint_dim)
