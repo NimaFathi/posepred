d51877c97c9e9a3143aa5d41c06905b020a7414c
Nima Fathi <nimafathinimafathi@gmail.com>
2021-11-01 03:33:10 +0330

    Fix: add normalizing in models

--- utils/normal.py
+++ utils/normal.py
@@ -2,10 +2,11 @@ import torch
 
 
 def normalize(in_tensor, mean, std):
+    device = 'cuda' if in_tensor.is_cuda else 'cpu'
     bs, frame_n, feature_n = in_tensor.shape
+    mean = torch.tensor(mean).to(device)
+    std = torch.tensor(std).to(device)
     keypoint_dim = mean.shape[0]
-    mean = torch.tensor(mean)
-    std = torch.tensor(std)
     assert mean.shape == std.shape
     assert feature_n % keypoint_dim == 0
     mean = mean.unsqueeze(0).repeat(bs, frame_n, feature_n // keypoint_dim)
@@ -15,10 +16,11 @@ def normalize(in_tensor, mean, std):
 
 
 def denormalize(in_tensor, mean, std):
+    device = 'cuda' if in_tensor.is_cuda else 'cpu'
     bs, frame_n, feature_n = in_tensor.shape
+    mean = torch.tensor(mean).to(device)
+    std = torch.tensor(std).to(device)
     keypoint_dim = mean.shape[0]
-    mean = torch.tensor(mean)
-    std = torch.tensor(std)
     assert mean.shape == std.shape
     assert feature_n % keypoint_dim == 0
     mean = mean.unsqueeze(0).repeat(bs, frame_n, feature_n // keypoint_dim)
