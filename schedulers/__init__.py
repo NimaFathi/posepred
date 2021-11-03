40d36b89dc7794094dd88dcee0d4b4af15a5d624
rmool <armin2saadat@gmail.com>
2021-11-03 09:41:14 +0330

    add step_lr scheduler.

--- schedulers/__init__.py
+++ schedulers/__init__.py
@@ -1,6 +1,6 @@
-from .reduce_lr_on_plateau import reduce_lr_on_plateau
-from .step_lr import step_lr
+from .reduce_lr_on_plateau import Reduce_LR_On_Plateau
+from .step_lr import Step_LR
 
-SCHEDULERS = {'reduce_lr_on_plateau': reduce_lr_on_plateau,
-              'step_lr': step_lr,
+SCHEDULERS = {'reduce_lr_on_plateau': Reduce_LR_On_Plateau,
+              'step_lr': Step_LR,
               }
--- schedulers/step_lr.py
+++ schedulers/step_lr.py
@@ -1,5 +1,10 @@
 import torch.optim as optim
 
 
-def step_lr(optimizer, args):
-    return optim.lr_scheduler.StepLR(optimizer, step_size=args.step_size, gamma=args.gamma)
+class Step_LR:
+    def __init__(self, optimizer, args):
+        self.scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=args.step_size, gamma=args.gamma,
+                                                   last_epoch=args.last_epoch, verbose=args.verbose)
+
+    def step(self, in_=None):
+        self.scheduler.step()
