f6125e0343583a89c087e7323b4956985302f3b3
Nima Fathi <nimafathinimafathi@gmail.com>
2021-11-16 18:33:34 +0330

    Feature: add color handler for visualization

--- visualization/visualizer.py
+++ visualization/visualizer.py
@@ -266,15 +266,15 @@ class Visualizer:
             (min_axes[i] - (max(range_axes) - range_axes[i]) / 2, max_axes[i] + (max(range_axes) - range_axes[i]) / 2)
             for i in range(len(max_axes))]
         axe.set_axis_off()
-        axe.set_box_aspect((max_axes[0] - min_axes[0], max_axes[1] - min_axes[1], max_axes[2] - min_axes[2]))
+        # axe.set_box_aspect((max_axes[0] - min_axes[0], max_axes[1] - min_axes[1], max_axes[2] - min_axes[2]))
         axe.set_aspect('auto')
         axe.view_init(elev=rotation_3D[self.dataset_name][0], azim=rotation_3D[self.dataset_name][1])
-        # axe.set_xlim(xmin=true_range[0][0],
-        #              xmax=true_range[0][1])
-        # axe.set_ylim(ymin=true_range[2][0],
-        #              ymax=true_range[2][1])
-        # axe.set_zlim(zmin=true_range[1][0],
-        #              zmax=true_range[1][1])
+        axe.set_xlim(xmin=true_range[0][0],
+                     xmax=true_range[0][1])
+        axe.set_ylim(ymin=true_range[2][0],
+                     ymax=true_range[2][1])
+        axe.set_zlim(zmin=true_range[1][0],
+                     zmax=true_range[1][1])
 
     @staticmethod
     def __scene_to_image(pose, cam_ext, cam_int):
