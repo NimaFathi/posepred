5a2d2b8327de826cbae4c761492020115b94f374
Nima Fathi <nimafathinimafathi@gmail.com>
2021-11-17 09:44:17 +0330

    Feature: crazy things

--- metrics/__init__.py
+++ metrics/__init__.py
@@ -1,9 +1,12 @@
-from .pose_metrics import ADE, FDE, local_ade, local_fde, VIM, VAM
+from .pose_metrics import ADE, FDE, local_ade, local_fde, VIM, VAM, local_ade_400, local_ade_1000, local_ade_2000
 from .mask_metrics import accuracy, f1_score, precision, recall
 
 POSE_METRICS = {'ADE': ADE,
                 'FDE': FDE,
                 'local_ade': local_ade,
+                'ade_400': local_ade_400,
+                'ade_1000': local_ade_1000,
+                'ade_2000': local_ade_2000,
                 'local_fde': local_fde,
                 'VIM': VIM,
                 'VAM': VAM}
--- metrics/pose_metrics.py
+++ metrics/pose_metrics.py
@@ -37,6 +37,16 @@ def local_ade(pred, target, dim, mask=None):
     local_target_pose = target_pose - target_pose[:, :, 0:1, :].repeat(1, 1, keypoints, 1)
     return ADE(local_pred_pose, local_target_pose, dim)
 
+def local_ade_400(pred, target, dim, mask=None):
+    return local_ade(pred, target, dim, mask)
+
+def local_ade_1000(pred, target, dim, mask=None):
+    return local_ade(pred, target, dim, mask)
+
+def local_ade_2000(pred, target, dim, mask=None):
+    return local_ade(pred, target, dim, mask)
+
+
 
 def local_fde(pred, target, dim, mask=None):
     bs, frames, feat = pred.shape
