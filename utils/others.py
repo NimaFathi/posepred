188a9a5c4dbf8cb058cb62cd966532c2aa90c472
rmool <armin2saadat@gmail.com>
2021-12-28 11:16:51 +0330

    move normal funcs to utils.others

--- utils/others.py
+++ utils/others.py
@@ -129,3 +129,31 @@ def qeuler(q, order, epsilon=0):
         raise
 
     return torch.stack((x, y, z), dim=1).view(original_shape)
+
+
+def normalize(in_tensor, mean, std):
+    device = 'cuda' if in_tensor.is_cuda else 'cpu'
+    bs, frame_n, feature_n = in_tensor.shape
+    mean = torch.tensor(mean).to(device)
+    std = torch.tensor(std).to(device)
+    keypoint_dim = mean.shape[0]
+    assert mean.shape == std.shape
+    assert feature_n % keypoint_dim == 0
+    mean = mean.unsqueeze(0).repeat(bs, frame_n, feature_n // keypoint_dim)
+    std = std.unsqueeze(0).repeat(bs, frame_n, feature_n // keypoint_dim)
+
+    return (in_tensor - mean) / std
+
+
+def denormalize(in_tensor, mean, std):
+    device = 'cuda' if in_tensor.is_cuda else 'cpu'
+    bs, frame_n, feature_n = in_tensor.shape
+    mean = torch.tensor(mean).to(device)
+    std = torch.tensor(std).to(device)
+    keypoint_dim = mean.shape[0]
+    assert mean.shape == std.shape
+    assert feature_n % keypoint_dim == 0
+    mean = mean.unsqueeze(0).repeat(bs, frame_n, feature_n // keypoint_dim)
+    std = std.unsqueeze(0).repeat(bs, frame_n, feature_n // keypoint_dim)
+
+    return (in_tensor * std) + mean
