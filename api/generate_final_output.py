f38c487f0cfcac9bd33737665df24478b5bb53c9
rmool <armin2saadat@gmail.com>
2021-12-29 22:07:19 +0330

    edit api.generate_final_output to prior version.

--- api/generate_final_output.py
+++ api/generate_final_output.py
@@ -2,8 +2,6 @@ import os
 import logging
 import hydra
 from omegaconf import DictConfig
-import json
-import torch
 
 from data_loader import get_dataloader
 from models import MODELS
@@ -37,50 +35,8 @@ def generate_output(cfg: DictConfig):
     cfg.save_dir = os.getcwd()
     setup_testing_dir(cfg.save_dir)
 
-    # 3DPW
-    # with open("/home/rmool/posepred/preprocessed_data/3dpw_test_in.json", "r") as read_file:
-    #     data = json.load(read_file)
-    # out_data = []
-    # for i in range(len(data)):
-    #     lp = []
-    #     for j in range(len(data[i])):
-    #         pose = torch.tensor(data[i][j]).unsqueeze(0).to(cfg.device)
-    #         inputs = {'observed_pose': pose}
-    #         outputs = model(inputs)
-    #         pred = outputs['pred_pose'].squeeze(0)
-    #         lp.append(pred.tolist())
-    #     out_data.append(lp)
-    # with open("/home/rmool/posepred/preprocessed_data/3dpw_test_out.json", 'w') as f:
-    #     json.dump(out_data, f)
-
-    with open("/home/rmool/posepred/preprocessed_data/posetrack_test_in.json", "r") as read_file:
-        data = json.load(read_file)
-    with open("/home/rmool/posepred/preprocessed_data/posetrack_test_masks_in.json", "r") as read_file:
-        data_m = json.load(read_file)
-    out_data = []
-    out_mask = []
-    for i in range(len(data)):
-        lp = []
-        lm = []
-        for j in range(len(data[i])):
-            pose = torch.tensor(data[i][j]).unsqueeze(0).to(cfg.device)
-            mask = torch.tensor(data_m[i][j]).unsqueeze(0).to(cfg.device)
-            inputs = {'observed_pose': pose}
-            outputs = model(inputs)
-            pred = outputs['pred_pose'].squeeze(0)
-            m = mask[:, -1:, :]
-            mask_preds = torch.cat((m, m, m, m, m, m, m, m, m, m, m, m, m, m), 1).squeeze(0)
-            lp.append(pred.tolist())
-            lm.append(mask_preds.detach().cpu().numpy().round().tolist())
-        out_data.append(lp)
-        out_mask.append(lm)
-    with open("/home/rmool/posepred/preprocessed_data/posetrack_test_out.json", 'w') as f:
-        json.dump(out_data, f)
-    with open("/home/rmool/posepred/preprocessed_data/posetrack_test_out_masks.json", 'w') as f:
-        json.dump(out_mask, f)
-
-    # output_enerator = Output_Generator(model, dataloader, cfg.save_dir, cfg.device)
-    # output_enerator.generate()
+    output_enerator = Output_Generator(model, dataloader, cfg.save_dir, cfg.device)
+    output_enerator.generate()
 
 
 if __name__ == '__main__':
