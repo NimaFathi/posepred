88cd2b9d5fd55f37a85b5a0db0c5e3c94be91250
Parham Saremi <parhaamsaremi@gmail.com>
2022-02-28 12:37:11 +0100

    update our_dataset.py

--- data_loader/our_dataset.py
+++ data_loader/our_dataset.py
@@ -12,24 +12,35 @@ logger = logging.getLogger(__name__)
 
 
 class OurDataset(Dataset):
-    def __init__(self, 
-            dataset_path, 
-            keypoint_dim, 
-            is_testing, 
-            use_mask, 
-            is_visualizing, 
-            use_expmap,
-            use_rotmat,
-            use_euler,
-            use_quaternion, 
-            normalize,
-            metadata_path):
+    def __init__(self,
+                 dataset_path,
+                 keypoint_dim,
+                 is_testing,
+                 use_mask,
+                 is_visualizing,
+                 use_expmap,
+                 use_rotmat,
+                 use_euler,
+                 use_quaternion,
+                 use_xyz,
+                 normalize,
+                 metadata_path,
+                 seq_rate,
+                 frame_rate,
+                 len_observed,
+                 len_future):
 
         self.normalize = normalize
         self.use_expmap = use_expmap
         self.use_rotmat = use_rotmat
         self.use_euler = use_euler
         self.use_quaternion = use_quaternion
+        self.use_xyz = use_xyz
+        total_len = (len_observed+len_future)*frame_rate
+        self.frame_rate = frame_rate
+        self.total_len = total_len
+        self.len_observed = len_observed
+        self.len_future = len_future
 
         if normalize:
             assert metadata_path, "Specify metadata_path when normalize is true."
@@ -42,38 +53,47 @@ class OurDataset(Dataset):
             self.std_pose = None
 
         data = list()
-        tensor_keys = [
-            'expmap_pose',
-            'pose'
+        self.tensor_keys = [
         ]
 
         if self.use_expmap:
-            tensor_keys.append('expmap_pose')
+            self.tensor_keys.append('expmap_pose')
 
         if self.use_rotmat:
-            tensor_keys.append('rotmat_pose')
+            self.tensor_keys.append('rotmat_pose')
 
         if self.use_euler:
-            tensor_keys.append('euler_pose')
+            self.tensor_keys.append('euler_pose')
 
         if self.use_quaternion:
-            tensor_keys.append('quaternion_pose')
+            self.tensor_keys.append('quaternion_pose')
 
+        if self.use_xyz:
+            self.tensor_keys.append('xyz_pose')
+
+        assert len(
+            self.tensor_keys) > 0, "please determine the kind(s) of pose(es) you want to use in config file"
+        if 'xyz_pose' in self.tensor_keys:
+            assert len(self.tensor_keys)==1, "you can't use xyz with other poses"
+
+        indexes = []
         with jsonlines.open(dataset_path) as reader:
             for seq in reader:
                 seq_tensor = {}
                 for k, v in seq.items():
-                    if k in tensor_keys:
+                    if k in self.tensor_keys:
                         seq_tensor[k] = torch.tensor(v, dtype=torch.float32)
-                        print(k, seq_tensor[k].shape)
+
                     else:
                         seq_tensor[k] = v
-
                 data.append(seq_tensor)
-
-        return
+                len_seq = seq_tensor[self.tensor_keys[0]].shape[0]
+                print("the sequence length is: ", len_seq)
+                indexes = indexes + [(len(data)-1, i)
+                                     for i in range(0, len_seq-total_len+1, seq_rate)]
 
         self.data = data
+        self.indexes = indexes
         self.keypoint_dim = keypoint_dim
         self.is_testing = is_testing
         self.use_mask = use_mask
@@ -81,69 +101,19 @@ class OurDataset(Dataset):
         self.use_expmap = use_expmap
         self.use_quaternion = use_quaternion
 
-        seq = self.data[0]
-        assert 'observed_pose' in seq.keys(), 'dataset must include observed_pose'
-        self.keypoints_num = int(seq['observed_pose'].shape[-1] / self.keypoint_dim)
-        self.obs_frames_num = seq['observed_pose'].shape[-2]
-        if not self.is_testing:
-            assert 'future_pose' in seq.keys(), 'dataset must include future_pose'
-            self.future_frames_num = seq['future_pose'].shape[-2]
-
     def __len__(self):
-        return len(self.data)
+        return len(self.indexes)
 
     def __getitem__(self, index):
-        seq = self.data[index]
-
-        outputs_keys = ['observed_pose']
-        if self.use_mask:
-            outputs_keys.append('observed_mask')
-        if not self.is_testing:
-            outputs_keys.append('future_pose')
-            if self.use_mask:
-                outputs_keys.append('future_mask')
-
-        outputs = dict()
-        for key in outputs_keys:
-            if key in seq.keys():
-                outputs[key] = seq[key]
-            else:
-                raise Exception('dataset must include ' + key)
+        data_index, seq_index = self.data[index]
+        seq = self.data[data_index][seq_index:seq_index+self.total_len]
+        outputs = {"observed_pose": {}, "future_pose": {}}
         
-        if self.use_expmap:
-            outputs['observed_expmap_pose'] = \
-                    torch.tensor(seq['observed_expmap_pose'])
-            outputs['future_expmap_pose'] = \
-                    torch.tensor(seq['future_expmap_pose'])
-
-        if self.use_rotmat:
-            outputs['observed_rotmat_pose'] = \
-                    torch.tensor(seq['observed_rotmat_pose'])
-            outputs['future_rotmat_pose'] = \
-                    torch.tensor(seq['future_rotmat_pose'])
-
-        if self.use_euler:
-            outputs['observed_euler_pose'] = \
-                    torch.tensor(seq['observed_euler_pose'])
-            outputs['future_euler_pose'] = \
-                    torch.tensor(seq['future_euler_pose'])
-
-        if self.use_quaternion:
-            outputs['observed_quaternion_pose'] = \
-                    torch.tensor(seq['observed_quaternion_pose'])
-            outputs['future_quaternion_pose'] = \
-                    torch.tensor(seq['future_quaternion_pose'])
-
-        if self.is_visualizing:
-            if 'observed_image_path' in seq.keys():
-                outputs['observed_image'] = seq['observed_image_path']
-            if 'future_image_path' in seq.keys():
-                outputs['future_image'] = seq['future_image_path']
-            if 'observed_cam_extrinsic' in seq.keys():
-                outputs['observed_cam_ex'] = torch.tensor(seq['observed_cam_extrinsic'])
-            if 'future_cam_extrinsic' in seq.keys():
-                outputs['future_cam_ex'] = torch.tensor(seq['future_cam_extrinsic'])
-            if 'cam_intrinsic' in seq.keys():
-                outputs['cam_in'] = torch.tensor(seq['cam_intrinsic'])
+        for k in self.tensor_keys:
+            temp_seq = seq[k]
+            s = temp_seq.shape
+            temp_seq = temp_seq.view(-1,self.frame_rate, s[2], s[3])[:, 0, :, :]
+            outputs['observed_pose'][k] = temp_seq[:self.len_observed]
+            outputs['future_pose'][k] = temp_seq[self.len_observed:]
 
         return outputs
