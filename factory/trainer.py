399295f4ab0a5b329eabdcbf919cf77c0966a8bd
Parham Saremi <parhaamsaremi@gmail.com>
2022-03-17 11:04:44 +0330

    fix minor bug in trainer and evaluator

--- factory/trainer.py
+++ factory/trainer.py
@@ -92,7 +92,7 @@ class Trainer:
 
                 future_metric_pose = data['future_pose']
                 if 'future_metric_pose' in data:
-                    future_metric_pose = data['pred_metric_pose']
+                    future_metric_pose = data['future_metric_pose']
 
                 metric_value = metric_func(
                     pred_metric_pose.to(self.args.device),
