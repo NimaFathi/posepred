5c068979517f49de4d9d607e870e2487e11d7e67
realmarv <zahratehraninasab@gmail.com>
2022-03-28 02:28:54 +0430

    added convert expmap to xyz in utils.others.py

--- utils/others.py
+++ utils/others.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 import cv2
+from torch.autograd.variable import Variable
 
 def pose_from_vel(velocity, last_obs_pose, stay_in_frame=False):
     device = 'cuda' if velocity.is_cuda else 'cpu'
@@ -63,6 +64,8 @@ def expmap_to_quaternion(e):
     w = np.cos(0.5 * theta).reshape(-1, 1)
     xyz = 0.5 * np.sinc(0.5 * theta / np.pi) * e
     return np.concatenate((w, xyz), axis=1).reshape(original_shape)
+
+
 
 def rotmat_to_expmap(action_sequence):
   """Convert exponential maps to rotmats.
@@ -243,3 +246,118 @@ def denormalize(in_tensor, mean, std):
     std = std.unsqueeze(0).repeat(bs, frame_n, feature_n // keypoint_dim)
 
     return (in_tensor * std) + mean
+
+def some_variables():
+    """
+    borrowed from
+    https://github.com/una-dinosauria/human-motion-prediction/blob/master/src/forward_kinematics.py#L100
+    We define some variables that are useful to run the kinematic tree
+    Args
+      None
+    Returns
+      parent: 32-long vector with parent-child relationships in the kinematic tree
+      offset: 96-long vector with bone lenghts
+      rotInd: 32-long list with indices into angles
+      expmapInd: 32-long list with indices into expmap angles
+    """
+
+    parent = np.array([0, 1, 2, 3, 4, 5, 1, 7, 8, 9, 10, 1, 12, 13, 14, 15, 13,
+                        17, 18, 19, 20, 21, 20, 23, 13, 25, 26, 27, 28, 29, 28, 31]) - 1
+
+    offset = np.array(
+        [0.000000, 0.000000, 0.000000, -132.948591, 0.000000, 0.000000, 0.000000, -442.894612, 0.000000, 0.000000,
+          -454.206447, 0.000000, 0.000000, 0.000000, 162.767078, 0.000000, 0.000000, 74.999437, 132.948826, 0.000000,
+          0.000000, 0.000000, -442.894413, 0.000000, 0.000000, -454.206590, 0.000000, 0.000000, 0.000000, 162.767426,
+          0.000000, 0.000000, 74.999948, 0.000000, 0.100000, 0.000000, 0.000000, 233.383263, 0.000000, 0.000000,
+          257.077681, 0.000000, 0.000000, 121.134938, 0.000000, 0.000000, 115.002227, 0.000000, 0.000000, 257.077681,
+          0.000000, 0.000000, 151.034226, 0.000000, 0.000000, 278.882773, 0.000000, 0.000000, 251.733451, 0.000000,
+          0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 99.999627, 0.000000, 100.000188, 0.000000, 0.000000,
+          0.000000, 0.000000, 0.000000, 257.077681, 0.000000, 0.000000, 151.031437, 0.000000, 0.000000, 278.892924,
+          0.000000, 0.000000, 251.728680, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 99.999888,
+          0.000000, 137.499922, 0.000000, 0.000000, 0.000000, 0.000000])
+    offset = offset.reshape(-1, 3)
+
+    rotInd = [[5, 6, 4],
+              [8, 9, 7],
+              [11, 12, 10],
+              [14, 15, 13],
+              [17, 18, 16],
+              [],
+              [20, 21, 19],
+              [23, 24, 22],
+              [26, 27, 25],
+              [29, 30, 28],
+              [],
+              [32, 33, 31],
+              [35, 36, 34],
+              [38, 39, 37],
+              [41, 42, 40],
+              [],
+              [44, 45, 43],
+              [47, 48, 46],
+              [50, 51, 49],
+              [53, 54, 52],
+              [56, 57, 55],
+              [],
+              [59, 60, 58],
+              [],
+              [62, 63, 61],
+              [65, 66, 64],
+              [68, 69, 67],
+              [71, 72, 70],
+              [74, 75, 73],
+              [],
+              [77, 78, 76],
+              []]
+
+    expmapInd = np.split(np.arange(4, 100) - 1, 32)
+
+    return parent, offset, rotInd, expmapInd
+
+def fkl_torch(angles, parent, offset, rotInd, expmapInd):
+    """
+    pytorch version of fkl.
+    convert joint angles to joint locations
+    batch pytorch version of the fkl() method above
+    :param angles: N*99
+    :param parent:
+    :param offset:
+    :param rotInd:
+    :param expmapInd:
+    :return: N*joint_n*3
+    """
+    n_a = angles.data.shape[0]
+    j_n = offset.shape[0]
+    p3d = Variable(torch.from_numpy(offset)).float().unsqueeze(0).repeat(n_a, 1, 1)
+    angles = angles[:, 3:].contiguous().view(-1, 3)
+
+    theta = torch.norm(angles, 2, 1)
+    r0 = torch.div(angles, theta.unsqueeze(1).repeat(1, 3) + 0.0000001)
+    r1 = torch.zeros_like(r0).repeat(1, 3)
+    r1[:, 1] = -r0[:, 2]
+    r1[:, 2] = r0[:, 1]
+    r1[:, 5] = -r0[:, 0]
+    r1 = r1.view(-1, 3, 3)
+    r1 = r1 - r1.transpose(1, 2)
+    n = r1.data.shape[0]
+    R = (torch.eye(3, 3).repeat(n, 1, 1).float() + torch.mul(
+        torch.sin(theta).unsqueeze(1).repeat(1, 9).view(-1, 3, 3), r1) + torch.mul(
+        (1 - torch.cos(theta).unsqueeze(1).repeat(1, 9).view(-1, 3, 3)), torch.matmul(r1, r1))).view(n_a, j_n, 3, 3)
+
+    for i in np.arange(1, j_n):
+        if parent[i] > 0:
+            R[:, i, :, :] = torch.matmul(R[:, i, :, :], R[:, parent[i], :, :]).clone()
+            p3d[:, i, :] = torch.matmul(p3d[0, i, :], R[:, parent[i], :, :]) + p3d[:, parent[i], :]
+    return p3d
+
+def expmap_to_xyz_torch(expmap):
+        """
+        convert expmaps to joint locations
+        :param expmap: N*99
+        :return: N*32*3
+        """
+        expmap[:, 0:6] = 0
+        parent, offset, rotInd, expmapInd = some_variables()
+        xyz = fkl_torch(expmap, parent, offset, rotInd, expmapInd)
+        return xyz
+
