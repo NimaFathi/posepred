ff41458956b9eb5e9c04d05e0edf97ecb14e3fde
realmarv <zahratehraninasab@gmail.com>
2022-03-31 20:46:48 +0430

    added sts-gcn edited loss.
    Co-authored-by: Helium-5 <mehrshad.mirmohammadi@gmail.com>

---
+++ models/spherical_tcn/sts_gcn_loss.py
@@ -0,0 +1,22 @@
+import numpy as np
+import torch
+import torch.nn as nn
+
+
+class STSGCNLoss(nn.Module):
+
+    def __init__(self, args):
+        super().__init__()
+
+        self.args = args
+
+    def forward(self, y_pred, y_true):
+        y_pred = y_pred['pred_pose']
+        y_pred = y_pred.view((-1, y_pred.shape[-1]))
+
+        y_true = y_true['future_pose']
+        y_true = y_true.view(y_true.shape[0] * y_true.shape[1], -1) # BT, JD
+        loss = torch.mean(torch.norm(y_true.contiguous().view(-1, 3) - y_pred.contiguous().view(-1, 3), 2, 1))
+        outputs = {'loss': loss}
+
+        return outputs
