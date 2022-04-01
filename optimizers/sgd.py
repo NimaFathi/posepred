20c9102f22d22770d57e7be4154edb563022f683
realmarv <zahratehraninasab@gmail.com>
2022-04-01 14:31:20 +0430

    add weight decay to sgd optimizer

--- optimizers/sgd.py
+++ optimizers/sgd.py
@@ -2,4 +2,4 @@ import torch.optim as optim
 
 
 def sgd(params, args):
-    return optim.SGD(params, lr=args.lr, momentum=args.momentum, dampening=args.dampening, nesterov=args.nesterov)
+    return optim.SGD(params, lr=args.lr, momentum=args.momentum, dampening=args.dampening, nesterov=args.nesterov, weight_decay=args.weight_decay)
