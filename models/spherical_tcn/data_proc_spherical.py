7408b319c41b517833c87f61d5fa495bb9d27836
realmarv <zahratehraninasab@gmail.com>
2022-04-02 01:24:48 +0430

    fix convert to spherical bug

--- models/spherical_tcn/data_proc_spherical.py
+++ models/spherical_tcn/data_proc_spherical.py
@@ -1,3 +1,4 @@
+
 import torch
 from torch import nn
 import numpy as np
@@ -41,20 +42,22 @@ class Preprocess(nn.Module):
     def forward(self, observed_pose):
         B, T, D = observed_pose.shape
         out = observed_pose.reshape(B, T, D//3, 3) # B, T, 32, 3
-        out = out[:, :, E] - out[:, :, S] # B, T, 25, 3
+        out = out[:, :, E] - out[:, :, S] # B, T, 22, 3
         out = self.xyz_to_spherical(out)
+        out = out.reshape(*out.shape[:-2], -1)
         return out
 
     def xyz_to_spherical(self, inputs):
-        # inputs: B, T, 25, 3
-        rho = torch.norm(inputs, dim=-1)
-        theta = torch.arctan(inputs[:, :, :, 1] / inputs[:, :, :, 0]).unsqueeze(3)
-        phi = torch.arccos(inputs[:, :, :, 2] / rho).unsqueeze(3)
-        rho = rho.unsqueeze(3)
-        out = torch.cat([rho, theta, phi], dim=3)
-
+        # inputs: B, T, 22, 3
+        rho = torch.norm(inputs, p=2, dim=-1)
+        theta = torch.arctan(inputs[..., 2] / inputs[..., 0]).unsqueeze(-1)
+        tol = 0
+        theta[inputs[..., 0] < tol] = theta[inputs[..., 0] < tol] + torch.pi
+        phi = torch.arccos(inputs[..., 1] / rho).unsqueeze(-1)
+        rho = rho.unsqueeze(-1)
+        out = torch.cat([rho, theta, phi], dim=-1)
+        out[out.isnan()] = 0
         return out
-
 
 class Postprocess(nn.Module):
     def __init__(self, args):
@@ -63,14 +66,14 @@ class Postprocess(nn.Module):
 
     def forward(self, observed_pose, pred_pose):
         B, T, D = pred_pose.shape # B, T, 66
-        observed_pose = observed_pose.reshape(*observed_pose.shape[:2], 32, 3)
+        observed_pose = observed_pose.reshape(*observed_pose.shape[:2], 32, 3) # B, T2, 32, 3 
         pred_pose = pred_pose.reshape(B, T, D//3, 3)
 
         
         x = torch.zeros([B, T, 32, 3]).to(self.args.device)
-        x[:, :, index_to_copy] = observed_pose[:, -1:, index_to_copy] # B, T, 96
+        x[:, :, index_to_copy] = observed_pose[:, -1:, index_to_copy] # B, T, 32, 3
         
-        xyz_vec = self.spherical_to_xyz(pred_pose)
+        xyz_vec = self.spherical_to_xyz(pred_pose) # B, T, 22, 3
         x[:, :, E] = xyz_vec
         for c in connect:
             x[:, :, c[1]] = x[:, :, c[0]] + x[:, :, c[1]]
@@ -82,9 +85,39 @@ class Postprocess(nn.Module):
     def spherical_to_xyz(self, inputs):
         # inputs: B, T, 22, 3 : rho, theta, phi
         
-        x = inputs[:, :, :, 0] * torch.sin(inputs[:, :, :, 2]) * torch.cos(inputs[:, :, :, 1])
-        y = inputs[:, :, :, 0] * torch.sin(inputs[:, :, :, 2]) * torch.sin(inputs[:, :, :, 1])
-        z = inputs[:, :, :, 0] * torch.cos(inputs[:, :, :, 2])
+        x = inputs[..., 0] * torch.sin(inputs[..., 2]) * torch.cos(inputs[..., 1])
+        y = inputs[..., 0] * torch.sin(inputs[..., 2]) * torch.sin(inputs[..., 1])
+        z = inputs[..., 0] * torch.cos(inputs[..., 2])
         x, y, z = x.unsqueeze(-1), y.unsqueeze(-1), z.unsqueeze(-1)
 
-        return torch.cat([x, y, z], dim=-1)
+        return torch.cat([x, z, y], dim=-1)
+
+
+class Namespace:
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+
+if __name__ == "__main__":
+    import jsonlines
+    args = Namespace(device='cuda')
+    preproc = Preprocess(args).to(args.device)
+    postproc = Postprocess(args).to(args.device)
+
+    path = '/home/zahra/workshop/posepred/preprocessed_data/stanford36m/train_total_xyz.jsonl'
+    sequences = []
+    with jsonlines.open(path) as reader:
+        i = 0
+        for seq in reader:
+            sequences.append(np.array(seq['xyz_pose']))
+            i += 1
+            if i == 1: break
+
+    sequence_gt = sequences[0] # T, 96
+    inputs = torch.tensor(sequence_gt[:1]).unsqueeze(0).to(args.device).float() # 1, 10, 96
+    inputs = inputs.reshape(1, 1, 32, 3)  
+    spherical = preproc.xyz_to_spherical(inputs)
+    outputs = postproc.spherical_to_xyz(spherical)
+    
+    print(torch.allclose(inputs, outputs, atol=1e-4, rtol=1e-4))
+
