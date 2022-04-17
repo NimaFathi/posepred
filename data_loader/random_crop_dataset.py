66d0cc0f6da92f730f683490657c8c28a7b7f9f1
realmarv <zahratehraninasab@gmail.com>
2022-04-17 18:23:06 +0430

    data_augmentation > transforms and Interpolate > RandomInterpolate

--- data_loader/random_crop_dataset.py
+++ data_loader/random_crop_dataset.py
@@ -8,7 +8,7 @@ import torch
 from torch.utils.data import Dataset
 
 from path_definition import PREPROCESSED_DATA_DIR
-from data_augmentation.interpolation import Interpolate
+from transforms.transforms import RandomInterpolate
 import random
 logger = logging.getLogger(__name__)
 
@@ -133,8 +133,7 @@ class RandomCropDataset(Dataset):
         self.use_mask = use_mask
         self.is_visualizing = is_visualizing
 
-        self.interpolate = Interpolate(0.9)
-        self.p = 0.5
+        self.interpolate = RandomInterpolate(0.9, 0.5)
 
     def __len__(self):
         return len(self.indexes)
@@ -156,8 +155,7 @@ class RandomCropDataset(Dataset):
         for k in output_keys:
             temp_seq = seq[k][seq_index:seq_index + self.total_len]
             temp_seq = temp_seq[::self.frame_rate]
-            if random.uniform(0, 1) < self.p:
-                temp_seq = self.interpolate(temp_seq)
+            temp_seq = self.interpolate(temp_seq)
             outputs["observed_" + k] = temp_seq[:self.len_observed]
             outputs["future_" + k] = temp_seq[self.len_observed:]
 
