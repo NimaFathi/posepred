7ceb21966841f43e1da2ff59dd74a595e27c1fe7
Seyedmehrshad Mirmohammadi <smirmoha@izar.ib0.cm.izar.local>
2022-04-19 11:23:39 +0200

    add random scale factor option

--- transforms/transforms.py
+++ transforms/transforms.py
@@ -5,19 +5,26 @@ from torch import nn
 import torch
 
 class RandomInterpolate(nn.Module):
-    def __init__(self, scale_factor, mode='cubic_spline'):
+    def __init__(self, scale_factor, mode='cubic_spline', scale_mode='constant'):
         super(RandomInterpolate, self).__init__()
         assert scale_factor < 1
         self.scale_factor = scale_factor
         self.mode = mode
+        self.scale_mode = scale_mode
         if mode == 'cubic_spline':
             self.interpolator = CubicSpline
 
     def forward(self, y):
         # y: ..., T, n_major_joints*keypoint_dim
+        if self.scale_mode == 'random':
+            scale_factor = random.uniform(self.scale_factor, 1)
+            scale_factor = round(scale_factor, 2)
+        else:
+            scale_factor = self.scale_factor
+
         x = np.arange(y.shape[-2])
         interpolator = self.interpolator(x, y, axis=-2)
-        new_x = np.arange(0, y.shape[-2], self.scale_factor)[:y.shape[-2]]
+        new_x = np.arange(0, y.shape[-2], scale_factor)[:y.shape[-2]]
         y = interpolator(new_x)
         return torch.tensor(y).float()
 
