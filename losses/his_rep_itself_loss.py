7e8938d6b1e5ac7e5b4a821404833a7a26ecd86a
alimohammadiamirhossein <alimohammadiamirhossein@gmail.com>
2022-04-30 15:11:03 +0430

    history repeats itself add itera

--- losses/his_rep_itself_loss.py
+++ losses/his_rep_itself_loss.py
@@ -10,6 +10,7 @@ class HisRepItselfLoss(nn.Module):
 
         self.args = args
         self.output_n = args.output_n
+        self.input_n = args.input_n
         self.seq_in = args.kernel_size
         self.device = args.device
         self.mode = args.un_mode
@@ -33,7 +34,7 @@ class HisRepItselfLoss(nn.Module):
             (self.joint_to_ignore * 3, self.joint_to_ignore * 3 + 1, self.joint_to_ignore * 3 + 2))
         self.joint_equal = np.array([13, 19, 22, 13, 27, 30])
         self.index_to_equal = np.concatenate((self.joint_equal * 3, self.joint_equal * 3 + 1, self.joint_equal * 3 + 2))
-        self.itera = 1
+        self.itera = args.itera
         self.action_dict = {
                 "walking": 0, 
                 "eating": 1, 
@@ -51,7 +52,7 @@ class HisRepItselfLoss(nn.Module):
                 "walkingdog": 13,
                 "walkingtogether": 14
         }
-
+        
         # self.idx = np.expand_dims(np.arange(self.seq_in + self.out_n), axis=1) + (
         #         self.out_n - self.seq_in + np.expand_dims(np.arange(self.itera), axis=0))
 
@@ -136,9 +137,13 @@ class HisRepItselfLoss(nn.Module):
         # print('mode', self.mode)
         # print('observed_loss', (p3d_out_all[:, :10, 0] - p3d_sup[:, :10]).sum())
         if self.mode == 'default':
-            loss_p3d = torch.mean(torch.norm(p3d_out_all[:, :, 0] - p3d_sup, dim=3))
+            if self.itera == 1:
+                # print(p3d_out_all.shape)
+                loss_p3d = torch.mean(torch.norm(p3d_out_all[:, :, 0] - p3d_sup, dim=3))
+            else:
+                # print(7, p3d_out_all.shape)
+                loss_p3d = torch.mean(torch.norm(p3d_out_all[:, :self.seq_in+10] - p3d_sup[:, :self.seq_in+10], dim=3))
         else:
-
             if 'A' in self.mode:
                 actions = torch.tensor([self.action_dict[a] for a in input_data['action']]).to(self.device)
             else:
@@ -150,14 +155,15 @@ class HisRepItselfLoss(nn.Module):
         p3d_out = model_outputs['pred_metric_pose']
         # print('p3d_out', p3d_out.shape)
         # print('loss mp', p3d_h36[:, -self.output_n:].shape, p3d_out.shape, joints//3)
+
         mpjpe_p3d_h36 = torch.mean(
             torch.norm(p3d_h36[:, -self.output_n:].reshape(
                 [-1, self.output_n, (joints // 3), 3]
             ) - p3d_out.reshape(
-                p3d_out.shape[0], p3d_out.shape[1], p3d_out.shape[2] // 3, 3), dim=3
+                p3d_out.shape[0], p3d_out.shape[1], joints // 3, 3), dim=3
             )
         )
-
+        # print(17, mpjpe_p3d_h36)
         outputs = {'loss': loss_p3d, 'mpjpe': mpjpe_p3d_h36}
         # print(outputs)
         if 'pred_mask' in model_outputs.keys():
