9fdada386b774d0311f6d172dc2cc22a920ec3d3
Parham Saremi <parhaamsaremi@gmail.com>
2022-07-11 09:37:20 +0200

    add no_step_lr

--- schedulers/__init__.py
+++ schedulers/__init__.py
@@ -1,8 +1,10 @@
 from .reduce_lr_on_plateau import Reduce_LR_On_Plateau
 from .step_lr import Step_LR
 from .multi_step_lr import MultiStepLR
+from .no_step_lr import No_Step_LR
 
 SCHEDULERS = {'reduce_lr_on_plateau': Reduce_LR_On_Plateau,
               'step_lr': Step_LR,
-              'multi_step_lr': MultiStepLR
+              'multi_step_lr': MultiStepLR,
+              'no_step_lr': No_Step_LR
               }
---
+++ schedulers/no_step_lr.py
@@ -0,0 +1,11 @@
+import torch.optim as optim
+
+
+class No_Step_LR:
+    def __init__(self, optimizer, args):
+        pass
+
+    def step(self, in_=None):
+        # self.scheduler.step()
+        pass
+
