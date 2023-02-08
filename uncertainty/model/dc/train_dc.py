Fix uncertainty calculation based on the library

uncertainty/model/dc/train_dc.py
@@ -4,7 +4,7 @@ from argparse import Namespace
 
 import numpy as np
 import torch.nn as nn
-from sklearn.cluster import KMeans
+#from sklearn.cluster import KMeans
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
@@ -166,14 +166,14 @@ def cluster(dataset, encoder: EncoderWrapper, k, device):
             hidden_state = encoder(data.to(device)).to(device)
             encoded_data = torch.cat((encoded_data, hidden_state), dim=0)
 
-    kmeans = KMeans(
-        n_clusters=k,
-        init='k-means++',
-        n_init=10,
-        max_iter=1000
-    )
+#    kmeans = KMeans(
+#        n_clusters=k,
+#        init='k-means++',
+#        n_init=10,
+#        max_iter=1000
+#    )
     encoded_data = encoded_data.to('cpu').detach().numpy()
     print('Initializing cluster centers with k-means...')
-    kmeans.fit(encoded_data)
+#    kmeans.fit(encoded_data)
     print('Clustering finished!')
-    return kmeans.cluster_centers_
+#    return kmeans.cluster_centers_

uncertainty/utils/prediction_util.py
@@ -1,11 +1,12 @@
 import torch
+from tqdm import tqdm
 from torch.utils.data import DataLoader
 
 from .dataset_utils import DIM, JOINTS_TO_INCLUDE, INCLUDED_JOINTS_COUNT
 from .functions import rescale_to_original_joints_count
 from ..model.sts_gcn.sts_gcn import STSGCN
 
-OUT_K, GT_K = 'output', 'gt'
+OUT_K, GT_K = 'outputs', 'ground_truths'
 PRED_MODELS = {'sts': STSGCN}
 PRED_MODELS_ARGS = {
     'sts': {'input_channels': DIM, 'input_time_frame': 10, 'output_time_frame': 25, 'st_gcnn_dropout': 0.1,
@@ -15,17 +16,14 @@ PRED_MODELS_ARGS = {
 def get_prediction_model_dict(model, data_loader: DataLoader, input_n: int, output_n: int, dataset_name: str,
                               dev='cuda') -> dict:
     prediction_dict = {OUT_K: [], GT_K: []}
-    for _, data_arr in enumerate(data_loader):
-        pose = data_arr[0].to(dev)
-        B = pose.shape[0]
-        inp = pose[:, :input_n, JOINTS_TO_INCLUDE[dataset_name]]. \
-            view(B, input_n, INCLUDED_JOINTS_COUNT[dataset_name] // DIM, DIM).permute(0, 3, 1, 2)
-        gt = pose[:, input_n:input_n + output_n, :]
+    pose_key = None
+    for data_arr in data_loader:
+        gt = data_arr["future_pose"]
+        B = gt.shape[0]
         if len(gt) == 1:
             gt = gt.unsqueeze(0)
         with torch.no_grad():
-            out = model(inp).permute(0, 1, 3, 2).contiguous().view(-1, output_n, INCLUDED_JOINTS_COUNT[dataset_name])
-            out = rescale_to_original_joints_count(out, gt, dataset_name)
+            out = model(dict(data_arr))["pred_pose"]
             prediction_dict[GT_K].append(gt)
             prediction_dict[OUT_K].append(out)
     prediction_dict[GT_K] = torch.concat(prediction_dict[GT_K], dim=0)

uncertainty/utils/uncertainty.py
@@ -3,9 +3,9 @@ import torch
 from ..model.dc.deep_clustering import DCModel
 from ..utils.functions import *
 from ..utils.dataset_utils import JOINTS_TO_INCLUDE, DIM, SCALE_RATIO, MPJPE_COEFFICIENT
-from ..utils.prediction_util import OUT_K, GT_K
+from ..utils.prediction_util import OUT_K
 
-LOSS_K, UNC_K = 'loss', 'uncertainty'
+UNC_K = 'uncertainty'
 
 
 def mpjpe_error(batch_pred, batch_gt):
@@ -20,21 +20,18 @@ def entropy_uncertainty(p):
     return torch.mean(out)
 
 
-def calculate_dict_uncertainty_and_mpjpe(dataset_name: str, model_dict: dict, dc_model: DCModel, dev='cuda') -> dict:
+def calculate_dict_uncertainty_and_mpjpe(dataset_name: str, model_dict: dict, dc_model: DCModel, batch_size: int, dev='cuda') -> dict:
     err = 0
     uncertainties = 0
-    print(model_dict[OUT_K].shape)
-    total_count, batch_size = model_dict[OUT_K].shape
+    total_count = len(model_dict[OUT_K])
     num_batches = int(total_count / batch_size)
     if (total_count % batch_size) != 0:
         num_batches += 1
     for k in range(num_batches):
         s_idx, e_idx = k * batch_size, min((k + 1) * batch_size, total_count)
         out = model_dict[OUT_K][s_idx: e_idx, :, :].to(dev)
-        gt = model_dict[GT_K][s_idx: e_idx, :, :].to(dev)
-        err += mpjpe_error(out, gt).cpu().detach().numpy() * (e_idx - s_idx)
         out = out[:, :, JOINTS_TO_INCLUDE[dataset_name]]
         with torch.no_grad():
             p, _ = dc_model(scale(out, SCALE_RATIO[dataset_name]))
             uncertainties += entropy_uncertainty(p) * (e_idx - s_idx)
-    return {LOSS_K: err * MPJPE_COEFFICIENT[dataset_name]/ total_count, UNC_K: uncertainties / total_count}
+    return {UNC_K: uncertainties / total_count}