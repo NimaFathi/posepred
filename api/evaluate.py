Fix uncertainty calculation based on the library

api/evaluate.py
@@ -44,10 +44,12 @@ def evaluate(cfg: DictConfig):
     evaluator = Evaluator(cfg, dataloader, model, loss_module, eval_reporter)
     evaluator.evaluate()
     if cfg.eval_uncertainty:
-        uncertainty_model = load_dc_model(cfg.dataset, cfg.uncertainty_load_path)
+        dataset_name = 'Human36m'
+        uncertainty_model = load_dc_model(dataset_name, cfg.uncertainty_load_path)
+        model_dict = torch.load(cfg.model_dict)
         uncertainty_evaluator = UncertaintyEvaluator(cfg, dataloader, model, uncertainty_model,
                                                      cfg.model.obs_frames_num, cfg.model.pred_frames_num,
-                                                     cfg.dataset, eval_reporter)
+                                                     dataset_name, model_dict, eval_reporter)
         uncertainty_evaluator.evaluate()
 
 