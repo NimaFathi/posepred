52d8d4169a5dae6f4623b335ac65a05e16e2c1c7
Yashar Zoroofchi <yashyzb@gmail.com>
2023-02-17 07:15:11 -0500

    Fixing saving uncertainty during traning & validation

--- factory/trainer.py
+++ factory/trainer.py
@@ -19,7 +19,7 @@ from os.path import join
 
 class Trainer:
     def __init__(self, args, train_dataloader, valid_dataloader, model, loss_module, optimizer, optimizer_args,
-                 scheduler, train_reporter, valid_reporter, uncertainty_evaluator=None):
+                 scheduler, train_reporter, valid_reporter, train_uncertainty_evaluator=None, validation_uncertainty_evaluator=None):
         self.args = args
         self.train_dataloader = train_dataloader
         self.valid_dataloader = valid_dataloader
@@ -32,7 +32,8 @@ class Trainer:
         self.valid_reporter = valid_reporter
         self.tensor_board = SummaryWriter(args.save_dir)
         self.use_validation = False if valid_dataloader is None else True
-        self.uncertainty_evaluator = uncertainty_evaluator
+        self.train_uncertainty_evaluator = train_uncertainty_evaluator
+        self.validation_uncertainty_evaluator = validation_uncertainty_evaluator
         
         mlflow.set_tracking_uri(join(args.mlflow_tracking_uri, 'mlruns') if args.mlflow_tracking_uri else join(ROOT_DIR, 'mlruns'))
         mlflow.set_experiment(args.experiment_name if args.experiment_name else args.model.type)
@@ -77,8 +78,8 @@ class Trainer:
                                 self.train_reporter,
                                 self.valid_reporter, self.args.save_dir, best_model=True)
                     self.best_model = False
-            if self.uncertainty_evaluator is not None:
-                self.__validate_uncertainty()
+            if self.validation_uncertainty_evaluator is not None:
+                self.__validate_uncertainty(train=False)
 
             if (epoch + 1) % self.args.snapshot_interval == 0 or (epoch + 1) == self.args.epochs:
                 save_snapshot(self.model, self.loss_module, self.optimizer, self.optimizer_args, epoch + 1,
@@ -175,6 +176,8 @@ class Trainer:
 
         self.train_reporter.epoch_finished(self.tensor_board, mlflow)
         self.train_reporter.print_values(logger, self.model.args.use_mask)
+        if self.train_uncertainty_evaluator is not None:
+            self.__validate_uncertainty(train=True)
 
     def __validate(self):
         self.model.eval()
@@ -236,5 +239,8 @@ class Trainer:
         self.valid_reporter.epoch_finished(self.tensor_board, mlflow)
         self.valid_reporter.print_values(logger, self.model.args.use_mask)
 
-    def __validate_uncertainty(self):
-        self.uncertainty_evaluator.evaluate()
+    def __validate_uncertainty(self, train=True):
+        if train:
+            self.train_uncertainty_evaluator.evaluate()
+        else:
+            self.validation_uncertainty_evaluator.evaluate()
--- utils/reporter.py
+++ utils/reporter.py
@@ -43,6 +43,8 @@ class Reporter:
                     self.history[key] = []
                     self.min_attrs[key] = float('inf')
             if counts is not None and key in counts.keys():
+#                print(value)
+#                print(self.attrs.get(key))
                 self.attrs.get(key).update(value, counts[key])
             else:
                 self.attrs.get(key).update(value, batch_size)
@@ -78,11 +80,27 @@ class Reporter:
         logger.info(str(msg))
         sys.stdout.flush()
 
+    def print_uncertainty_values(self, logger, unc_k):
+        msg = self.state + '-epoch' + str(len(self.history['time'])) + ': '
+        for key, value in self.history.items():
+            if not unc_k in key:
+                continue
+            msg += key + ': %.5f, ' % value[-1]
+        logger.info(str(msg))
+        sys.stdout.flush()
+
     def save_data(self, use_mask, save_dir):
         for key, value in self.history.items():
             if not use_mask and 'mask' in key:
                 continue
             with open(os.path.join(save_dir, 'metrics_history', '_'.join((self.state, key)) + '.json'), "w") as f:
+                json.dump(value, f, indent=4)
+
+    def save_uncertainty_data(self, unc_k, save_dir):
+        for key, value in self.history.items():
+            if not unc_k in key:
+                continue
+            with open(os.path.join(save_dir, 'uncertainty_history', '_'.join((self.state, key)) + '.json'), "w") as f:
                 json.dump(value, f, indent=4)
 
     def print_mean_std(self, logger, use_mask):
@@ -108,14 +126,11 @@ class Reporter:
             logger.info(' |'.join([action.ljust(15)] + [str(np.around(a, 4)).center(15) for a in to_print]))
 
     def print_pretty_uncertainty(self, logger, unc_k):
-        actions = ["all"]
-        actions = list(sorted(actions))
-        logger.info(' |'.join(["actions".ljust(15)] + ["Uncertainty".center(15)]))
-        logger.info("_" * 20)
-        for action in actions:
-            to_print = []
-            to_print.append(np.mean(self.history.get(f'{unc_k}_{action}')))
-            logger.info(' |'.join([action.ljust(15)] + [str(np.around(to_print, 4)).center(15)]))
+        logger.info("_" * 20 * (2))
+        to_print = []
+        to_print.append(np.mean(self.history.get(unc_k)))
+        logger.info(' |'.join(["all".ljust(15)] + [str(np.around(a, 4)).center(15) for a in to_print]))
+
 
     def save_csv_metrics(self, use_mask, metrics, addr):
         actions = []
@@ -137,6 +152,18 @@ class Reporter:
             df_temp = pd.DataFrame(out_dict)
             out = pd.concat([out, df_temp], ignore_index=True, axis=0)
         out.to_csv(addr)
+
+
+    def save_csv_uncertainty(self, unc_k, addr):
+        out = pd.DataFrame(columns=["action"] + list(unc_k))
+
+        to_print = []
+        out_dict = {}
+        out_dict[unc_k] = [np.mean(self.history.get(unc_k))]
+        out_dict["action"] = "all"
+        out = pd.DataFrame(out_dict)
+        out.to_csv(addr)
+
 
     @staticmethod
     def save_plots(use_mask, save_dir, train_history, validiation_history, use_validation):
