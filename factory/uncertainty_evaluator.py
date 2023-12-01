Minor fixes for uncertainty calculation if not scaled.

factory/uncertainty_evaluator.py
@@ -60,12 +60,12 @@ class UncertaintyEvaluator:
 
                 for action in actions:
                     if action == "all":
-                        metric_value = calculate_pose_uncertainty(future_metric_pose.to(self.device),
+                        metric_value = calculate_pose_uncertainty(pred_metric_pose.to(self.device),
                                                                   self.uncertainty_model,
                                                                   self.args.dataset_name)
                     else:
                         indexes = np.where(np.asarray(data['action']) == action)[0]
-                        metric_value = calculate_pose_uncertainty(future_metric_pose.to(self.device)[indexes],
+                        metric_value = calculate_pose_uncertainty(pred_metric_pose.to(self.device)[indexes],
                                                                   self.uncertainty_model,
                                                                   self.args.dataset_name)
                         dynamic_counts[f'UNCERTAINTY_{action}'] = len(indexes)