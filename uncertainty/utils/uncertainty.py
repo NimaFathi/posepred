955377358f97ec8977278b69b33acc64da432b2a
yasharzb <yasharzb@ce.sharif.edu>
2023-12-01 20:27:11 +0330

    Minor fixes for uncertainty calculation if not scaled.

--- uncertainty/utils/uncertainty.py
+++ uncertainty/utils/uncertainty.py
@@ -14,8 +14,6 @@ def entropy_uncertainty(p, as_list=False):
 
 def calculate_pose_uncertainty(prediction_pose, dc_model, dataset_name: str):
     uncertainties = 0
-    #         uncertainties += entropy_uncertainty(p) * (e_idx - s_idx)
-    # return {LOSS_K: err * MPJPE_COEFFICIENT[dataset_name]/ total_count, UNC_K: uncertainties / total_count}
     with torch.no_grad():
         p, _ = dc_model(scale(prediction_pose[:, :, JOINTS_TO_INCLUDE[dataset_name]], SCALE_RATIO[dataset_name]))
         uncertainties += entropy_uncertainty(p)
